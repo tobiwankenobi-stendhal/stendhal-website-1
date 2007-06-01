cf1ede6742d2237cb07bc58d0df775bd60da692f
miguel <arianne_rpg@users.sourceforge.net>
2007-06-01 16:41:02 +0000

    Fixed SQL syntax

--- stendhal_website.sql
+++ stendhal_website.sql
@@ -1,8 +1,38 @@
 create table screenshots (
-  id unsigned int auto_increment not null,
+  id int auto_increment not null,
   url varchar(256),
   description text,
-  date timestamp default CURRENT_TIMESTAMP,
+  created timestamp default CURRENT_TIMESTAMP,
+  approved boolean default false,
   
   primary key(id)
-)
+);
+
+insert into screenshots values(null, "screenshots/screenshot.jpg", "Test image", null, null);
+
+
+create table events (
+  id int auto_increment not null,
+  date datetime not null,
+  type varchar(32),
+  location varchar(32),
+  shortDescription varchar(256),
+  extendedDescription text,
+  
+  created timestamp default CURRENT_TIMESTAMP,
+  
+  primary key(id)
+);
+
+insert into events values(null, "2007/06/01 18:50", "raid", "miguel's house", "Website reading from database", "No extended description", null);
+insert into events values(null, "2007/06/01 21:50", "raid", "Semos", "Goblins!", "No extended description", null);
+
+create table event_images (
+  id int auto_increment not null,
+  event_id int not null,
+  url varchar(256),
+  description text,
+  created timestamp default CURRENT_TIMESTAMP,
+  
+  primary key(id, event_id)
+);
