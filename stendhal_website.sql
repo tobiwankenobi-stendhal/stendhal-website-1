7d03b399f4f4fa65e67240d02226bc1c1d0307c3
miguel <arianne_rpg@users.sourceforge.net>
2007-06-01 17:09:16 +0000

    Added news and news_image tables.

--- stendhal_website.sql
+++ stendhal_website.sql
@@ -36,3 +36,38 @@ create table event_images (
   
   primary key(id, event_id)
 );
+
+create table news (
+  id int auto_increment not null,
+  title varchar(256),
+  shortDescription varchar(256),
+  extendedDescription text,
+  
+  created timestamp default CURRENT_TIMESTAMP,
+  
+  primary key(id)
+);
+
+insert into news values(null, "Stendhal BETA Testers server", "one line description one line description one line description one line description ", 
+"An important milestone for 0.70<br/>
+Welcome to this beta server.
+<p/>
+Please if you find any problem report it at <a href=\"http://sourceforge.net/tracker/?func=add&group_id=1111&atid=101111\">http://sourceforge.net/tracker/?func=add&group_id=1111&atid=101111</a>.
+<p/>
+We are interested in testing:<ul>
+<li>Maps</li>
+<li>Monster</li>
+<li>New items</li>
+<li>Quests</li>
+</ul>
+Please if you have any suggestion, post it at http://sourceforge.net/tracker/?func=add&group_id=1111&atid=351111", null);
+
+create table news_images (
+  id int auto_increment not null,
+  news_id int not null,
+  url varchar(256),
+  description text,
+  created timestamp default CURRENT_TIMESTAMP,
+  
+  primary key(id, news_id)
+);
