cf009f5c479e1eae2be484b2a94b9a7662934af5
miguel <arianne_rpg@users.sourceforge.net>
2007-06-07 09:10:29 +0000

    Wrote a bit of Events extended description. I miss the CSS.

--- content/scripts/event.php
+++ content/scripts/event.php
@@ -1,3 +1,18 @@
 <?php
-
+$event_id=$_REQUEST["event_id"];
+$events=getEvents('where id='.$event_id);
+$choosen=$events[0];
 ?>
+
+  <?php startBox($choosen->oneLineDescription); ?>
+    <div>
+      <div><b>Type: </b><?php echo $choosen->type; ?></div>
+      <div><b>Place: </b><?php echo $choosen->location; ?></div>
+      <div><b>Date: </b><?php echo $choosen->date; ?></div>
+    </div>
+    <?php echo $choosen->extendedDescription; ?>
+    
+    <?php foreach($choosen->images as $image) { ?>
+      <img src="<?php echo $image; ?>" alt="Raid image"/>
+    <?php } ?>
+  <?php endBox(); ?>
