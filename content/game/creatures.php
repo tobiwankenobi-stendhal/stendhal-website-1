a6100a985e967d39b8cb9e7882812d3dd33a9915
miguel <arianne_rpg@users.sourceforge.net>
2007-06-08 22:22:31 +0000

    Creatures :) Better rendering... tomorrow i will do the detailed info for each.

--- content/game/creatures.php
+++ content/game/creatures.php
@@ -1,26 +1,41 @@
 <?php
 
+define('AMOUNT',29);
+
+if(isset($_REQUEST['base'])) {
+  $base=$_REQUEST['base'];
+} else {
+  $base=0;
+}
+
 $monsters=getMonsters();
 $classes=Monster::getClasses();
 
 startBox('Creatures');
-foreach($classes as $class=>$idontcare) {
-  echo '<div><h1>'.$class.'</h1></div>';
-  foreach($monsters as $m) {
-    if($class==$m->class) {
-      echo '<div>';
+if($base-AMOUNT>=0) {
+echo '<div class="creatures_less">
+      <a href="?id=content/game/creatures&base='.($base-AMOUNT).'">Previous creatures</a>
+      </div>';
+}
+
+echo '<div style="position: relative; min-height: auto;">';
+
+  for($i=$base;$i<min(sizeof($monsters),$base+AMOUNT);$i++) {
+      $m=$monsters[$i];
+      
+      echo '<div class="creature">';
       echo '  <img src="'.$m->gfx.'" alt="'.$m->name.'"/>';
-      echo '  <div>'.$m->name.'</div>';
-      echo '  <div>'.$m->description.'</div>';
-      echo '  <div>'.$m->level.'</div>';
-      echo '  <div>';
-      foreach($m->attributes as $k=>$v) {
-        echo '  <div><span>'.$k.'</span><span>'.$v.'</span></div>';
-      }
-      echo '  </div>';
+      echo '  <div class="creature_name">'.$m->name.'</div>';
+      echo '  <div>Level '.$m->level.'</div>';
       echo '</div>';
-    }
   }
+
+echo '</div><div style="clear: left;"></div>';
+
+if($base+AMOUNT<sizeof($monsters)) {
+echo '<div class="creatures_more">
+      <a href="?id=content/game/creatures&base='.($base+AMOUNT).'">Next creatures</a>
+      </div>';
 }
 
 endBox();
