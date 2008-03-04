52dbf009a4b13c0c901e8a107f7329723515587f
kymara <kymara@users.sourceforge.net>
2008-03-04 10:35:47 +0000

    full fledged -> fully fledged

--- content/main.php
+++ content/main.php
@@ -1,6 +1,6 @@
 <div id="oneLineDescription">
    <img src="images/playit.gif" alt="play stendhal"/>
-   <span>Stendhal is a full fledged free open source multiplayer online adventures game (MMORPG) developed using the Arianne game system.</span>
+   <span>Stendhal is a fully fledged free open source multiplayer online adventures game (MMORPG) developed using the Arianne game system.</span>
 </div>
 <div id="newsArea">
   <?php
