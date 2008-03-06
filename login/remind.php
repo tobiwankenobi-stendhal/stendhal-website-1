be213f47ab47cb3414f9789a846e34a7a288e2f8
miguel <arianne_rpg@users.sourceforge.net>
2008-03-06 20:31:06 +0000

    Explaining a bit the problem so I know how to do it :)

--- login/remind.php
+++ login/remind.php
@@ -2,6 +2,12 @@
 
 include_once('login_function.php');
 
+/*
+   It has two alternatives:
+   a) User click new password and we email a md5(rand()) to him IF the account is registered with stendhal.
+   b) The user confirms the link and we effectively change the password.
+ */
+
 startBox("Forgot your password?");
 ?>
 In case you have forgotten your new password or your account information we can send you it to your email account that you used to create your stendhal account.<p>
