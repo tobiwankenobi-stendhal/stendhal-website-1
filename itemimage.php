5f2fa2f22fe32137ba313de97cfcee09d79b6e36
miguel <arianne_rpg@users.sourceforge.net>
2008-03-06 12:29:16 +0000

    Added headers about AGPL

--- itemimage.php
+++ itemimage.php
@@ -1,15 +1,22 @@
 <?php
-/***************************************************************************
- *                      (C) Copyright 2008 - Stendhal                      *
- ***************************************************************************
- ***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+/*
+    Stendhal website - a website to manage and ease playing of Stendhal game
+    Copyright (C) 2008  Miguel Angel Blanch Lardin
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
  
 header("Content-type: image/png");
 $url = $_GET['url'];
