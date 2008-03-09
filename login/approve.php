353e37a62ff2e35373e4b6bd7fefbd3a15fb13de
miguel <arianne_rpg@users.sourceforge.net>
2008-03-09 00:24:37 +0000

    Approve password reset php script

---
+++ login/approve.php
@@ -0,0 +1,73 @@
+<?php
+include_once('mysql.php');
+
+function createRandomPassword() {
+    $chars = "abcdefghijkmnopqrstuvwxyz023456789";
+    $i = 0;
+    $pass = '' ;
+    $amount=strlen($chars);
+
+    while ($i <= 7) {
+        $num = rand() % $amount;
+        $pass = $pass . $chars[$num];
+        $i++;
+    }
+    
+    return $pass;
+}
+
+if(!isset($_GET["sign"])) {
+  die('You didn\'t fill in a required field.');
+  }
+
+$signature=$_GET["sign"];
+$signature=mysql_real_escape_string($signature);
+
+/*
+ * Get the user name from the username<->hash relation
+ */
+$query='select username from remind_password where confirmhash="'.$signature.'"';
+$result = mysql_query($query, getWebsiteDB());
+
+if(mysql_numrows($result)!=1) {
+  mysql_free_result($result);
+
+  startBox("No such username");
+    ?>
+    We are unable to find a valid username associated to that email account.<p>
+    Your password can not be reset.
+    <p>
+    Back to <a href="?">Main</a>
+  <?php
+  endBox();
+} else {
+  $row=mysql_fetch_assoc($result);
+  $username=$row["username"];
+
+  mysql_free_result($result);
+  
+  /* TODO: Remove the entry */
+
+  /*
+   * Create a random password for it and set it.
+   */
+  $newpassword=createRandomPassword();
+
+  $md5newpass = strtoupper(md5($newpassword));
+  $q = "update account set password='$md5newpass' where username = '$username'";
+  $result = mysql_query($q,getGameDB());
+
+  /*
+   * Show user the new password.
+   */
+  startBox("New password generated");
+    ?>
+    Per your request we have reset the password of your account "<b><?php echo $username; ?></b>".<br>
+    Your new password is "<b><?php echo $newpassword; ?></b>".
+    <p>
+    Store it on a secure place.
+    <?php
+  endBox();
+  }   
+
+?>
