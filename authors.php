a0e952224f459a6ade785b234bcb34f03cc3a68d
miguel <arianne_rpg@users.sourceforge.net>
2008-03-12 16:54:38 +0000

    Added three new authors.

--- authors.php
+++ authors.php
@@ -6,6 +6,9 @@
 $authors= array(
   "miguel" => "08130401",
   "kymara" => "23071707",
+  "dontbugme" => "00000101",
+  "hendrik" => "06129901",
+  "tigertoes" => "36115012",
   /* Keep adding them */
 );
 
