3191187849107f6f8bc60eef064963c51d005e2f
miguel <arianne_rpg@users.sourceforge.net>
2008-03-27 18:36:33 +0000

    Added new files. BTW some od the old images commits are not commited in binary mode.

---
+++ content/offline.php
@@ -0,0 +1,6 @@
+<?php startBox("Server is offline"); ?>
+The server is right now offline.
+<p>
+This may be a desired behaviour in case of an update or it may be the result of any kind of problem.
+Please report it here so we are (even more) aware.
+<?php endBox(); ?>
