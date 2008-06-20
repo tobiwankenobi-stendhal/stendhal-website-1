7ba2cd11afd5e42f1277ce6c1fd3cff491f9dd2e
kymara <kymara@users.sourceforge.net>
2008-06-20 12:14:31 +0000

    keep the minature world map picture on arianne.sf.net so we onlyhave to update one each time (sanity..)

--- content/game/atlas.php
+++ content/game/atlas.php
@@ -1,5 +1,5 @@
 <?php startBox("World of Stendhal"); ?>
-<img class="screenshot" src="images/game/world_map_stendhal.jpg" alt="Miniature view of stendhal world map"/>
+<img class="screenshot" src="http://arianne.sourceforge.net/screens/stendhal/worldsmall.png" alt="Miniature view of stendhal world map"/>
 
 <div class="title">Extended info</div>
 You can obtain more info and more detailed views at our Wiki at <a href="http://arianne.sourceforge.net/wiki/index.php?title=StendhalAtlas">http://arianne.sourceforge.net/wiki/index.php?title=StendhalAtlas</a> 
