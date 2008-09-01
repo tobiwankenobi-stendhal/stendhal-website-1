bf101e9af45b20c45e15f16b1144ad431f0ddeb7
kymara <kymara@users.sourceforge.net>
2008-09-01 07:40:13 +0000

    edit the offline page for grammar, and provide a link to #arianne through the mibbit service for reporting server problems

--- content/offline.php
+++ content/offline.php
@@ -1,6 +1,6 @@
 <?php startBox("Server is offline"); ?>
-The server is right now offline.
+The server is offline right now.
 <p>
-This may be a desired behaviour in case of an update or it may be the result of any kind of problem.
-Please report it here so we are (even more) aware.
+This may be the desired behaviour, in case of an update, or it may be the result of some kind of problem.
+Please report it at <a href="http://embed.mibbit.com/?server=irc.freenode.net&channel=%23arianne">#arianne on irc.freenode.net</a>, if the channel topic there doesn't already mention it.
 <?php endBox(); ?>
