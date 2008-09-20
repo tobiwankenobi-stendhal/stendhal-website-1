bebfcf14fe0629620c99e03852bb0e064e921cfa
Hendrik Brummermann <nhnb@users.sourceforge.net>
2008-09-20 13:48:17 +0000

    read item information using the stendhal_website php scripts

--- mediawiki/stendhalDataInclude.php
+++ mediawiki/stendhalDataInclude.php
@@ -2,6 +2,10 @@
 if (!defined('MEDIAWIKI')) {
 	die("<b>Stendhal Data Include</b> is a MediaWiki extension not intended to be used on its own.");
 }
+
+require_once($IP.'/../scripts/xml.php');
+require_once($IP.'/../scripts/items.php');
+
 $wgExtensionFunctions[] = "stendhalDataIncludeSetup";
 $wgExtensionCredits['other'][] = array(
 	'version' => '0.1',
@@ -14,8 +18,49 @@ $wgExtensionCredits['other'][] = array(
 		. 'type="default" <em>or</em> type="mouseover" <em>or</em> type="include"<br>'
 );
 
+/*
+object(Item)#256 (6) {
+  ["name"]=>
+  string(13) "rod of the gm"
+  ["description"]=>
+  string(129) "You see a rod of the GM.  This rod is reserved for those with special powers, be careful not to let it fall into the wrong hands."
+  ["class"]=>
+  string(4) "club"
+  ["gfx"]=>
+  string(59) "itemimage.php?url=data/sprites/items/club/rod_of_the_gm.png"
+  ["attributes"]=>
+  array(3) {
+    ["atk"]=>
+    string(3) "100"
+    ["def"]=>
+    string(3) "100"
+    ["rate"]=>
+    string(1) "2"
+  }
+  ["equipableat"]=>
+  NULL
+}
+*/
+
+function getItemByName($name) {
+	$items = getItems();
+
+	foreach($items as $item) {
+		if($item->name==$name) {
+			return $item;
+		}
+	}
+	return NULL;
+}
+
 function stendhalDataIncludeItem($input, $argv, &$parser) {
-	return "hallo";
+	$item = getItemByName($input);
+	var_dump($item);
+	if ($item == NULL) {
+		return '&lt;item not found&gt;';
+	}
+
+	return $item->description;
 }
 
 function stendhalDataIncludeSetup() {
