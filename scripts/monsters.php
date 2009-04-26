9f52f3ad95fc6ede7d9ed1741a83d9a9afce22de
kymara <kymara@users.sourceforge.net>
2009-04-26 17:06:30 +0000

    oops sorry make both parts use TOD_DAYS not just one!

--- scripts/monsters.php
+++ scripts/monsters.php
@@ -132,7 +132,7 @@ class Monster {
      */
     $result = mysql_query('
     select 
-      dayofyear(timedate) - dayofyear(NOW()) as day_offset, 
+      TO_DAYS(timedate) - TO_DAYS(NOW()) as day_offset, 
       count(*) as amount 
     from gameEvents 
     where 
