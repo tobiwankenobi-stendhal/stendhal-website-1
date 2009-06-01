a3cd805b1857fdd8a8db21fa29ca81b1767aafac
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-06-01 18:15:33 +0000

    display list of npcs

---
+++ content/game/npcs.php
@@ -0,0 +1,54 @@
+<?php
+/*
+ Stendhal website - a website to manage and ease playing of Stendhal game
+ Copyright (C) 2008  Miguel Angel Blanch Lardin
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+$npcs=NPC::getNPCs();
+?>
+ 
+<div style="float: left; width: 100%"><?php
+
+startBox('NPCs');
+?>
+<form method="get" action="">
+  <input type="hidden" name="id" value="content/scripts/npc">
+  <input type="text" name="name" maxlength="60">
+  <input type="submit" name="sublogin" value="Search">
+</form>
+<div>
+  <?php echo sizeof($monsters); ?> NPCs so far.
+</div>
+
+<?php 
+
+foreach($npcs as $npc) {
+	echo '<div class="npc"><a class="npc" href="?id=content/scripts/monster&name='.$npc->name.'&exact">';
+	echo '  <img class="npc" src="'.$npc->imagefile.'" alt="'.$npc->name.'"/>';
+	echo '  <div class="npc_name">'.$npc->name.'</div>';
+	echo ' </a>';
+	echo '  <div class="npc_level">Level '.$npc->level.'</div>';
+	echo '</div>';
+}
+?>
+<div style="clear: left;"></div>
+<?php
+
+endBox();
+?>
+</div>
+<div style="clear: left;"></div>
+</div>
