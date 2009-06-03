d4f4760e895457ab21248569be9cbf5344f421cc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-06-03 19:19:32 +0000

    added search function

--- content/admin/playerhistory.php
+++ content/admin/playerhistory.php
@@ -2,19 +2,30 @@
 	if(getAdminLevel()<100) {
  		die("Ooops!");
 	}
-	startBox('Player History');
-
 	$name=$_REQUEST["name"];
-
-	echo '<table class="prettytable"><tr><th>time</th><th>source</th><th>event</th><th>parameters</th></tr>';
-
-	$history = PlayerHistoryEntry::getPlayerHistoryEntriesForPlayer($name);
-	foreach ($history as $entry) {
-		echo '<tr><td>'.$entry->timedate.'</td><td>'.$entry->source.'</td><td>'.$entry->event.'</td><td>'.$entry->param.'</td></tr>';
-	}
-	echo '</table>';
+	startBox('Search Player');
 ?>
-<p><b>These logs are for administrators eyes ONLY and should not be copied or pasted to others.</b></p>
+	<form method="get" action="">
+		<input type="hidden" name="id" value="content/admin/playerhistory">
+		<input type="text" name="name" maxlength="60" value="<?php if (isset($name)) {echo htmlspecialchars($name);}?>">
+		<input type="submit" name="sublogin" value="Search">
+	</form>
 <?php
-	endBox() 
+	endBox(); 
+
+	if (isset($name)) {
+		startBox('History for Player '. htmlspecialchars($name));
+
+		echo '<table class="prettytable"><tr><th>time</th><th>source</th><th>event</th><th>parameters</th></tr>';
+
+		$history = PlayerHistoryEntry::getPlayerHistoryEntriesForPlayer($name);
+		foreach ($history as $entry) {
+			echo '<tr><td>'.htmlspecialchars($entry->timedate).'</td><td>'.htmlspecialchars($entry->source)
+				.'</td><td>'.htmlspecialchars($entry->event).'</td><td>'.htmlspecialchars($entry->param).'</td></tr>';
+		}
+		echo '</table>';
+		echo '<p><b>These logs are for administrators eyes ONLY and should not be copied or pasted to others.</b></p>';
+
+		endBox(); 
+	}
 ?>
