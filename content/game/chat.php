06d9941edccf2eb28ad7d07250c8eef1945ea34c
kymara <kymara@users.sourceforge.net>
2009-06-12 15:17:47 +0000

    add a page about chat in irc, with links to mibbit. the irc logs could be kept here one day if liked.

---
+++ content/game/chat.php
@@ -0,0 +1,18 @@
+<?php startBox("Chat to other users and developers"); ?>
+You can get an IRC client and connect to:
+
+<p><b>irc.freenode.net</b> (see <a href="http://freenode.net">http://freenode.net</a> for more information) then
+<ul> 
+<li><b>#arianne-chat</b> which is for friendly chatter </li>
+<li> <b>#arianne</b> which is for technical discussion, getting help and planning.</li>
+</ul>
+<p>
+Alternatively, you can simply use mibbit's webchat service, below. Feel free to change the nick to for example, your player name. 
+<ul> 
+<li>
+<a href="http://embed.mibbit.com/?server=irc.freenode.net&channel=%23arianne-chat">#arianne-chat</a></li>
+<li>
+<a href="http://embed.mibbit.com/?server=irc.freenode.net&channel=%23arianne">#arianne</a></li>
+</ul>
+If you are new to IRC it is well worth reading this <a href="http://www.irchelp.org/irchelp/new2irc.html">short guide</a> before you join. In particular the section on talking, and entering commands, and the section 'Some advice' may be helpful.
+<?php endBox(); ?>
