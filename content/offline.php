070f2551e484436d805da92fae252717a7643024
kymara <kymara@users.sourceforge.net>
2009-07-10 06:43:20 +0000

    use correct webchat service for reporting offline problems - bug reported by bluelads4 thank you

--- content/offline.php
+++ content/offline.php
@@ -2,5 +2,5 @@
 The server is offline right now.
 <p>
 This may be the desired behaviour, in case of an update, or it may be the result of some kind of problem.
-Please report it at <a href="http://embed.mibbit.com/?server=irc.freenode.net&channel=%23arianne">#arianne on irc.freenode.net</a>, if the channel topic there doesn't already mention it.
+Please report it at <a href="http://webchat.freenode.net/?channels=arianne">#arianne on irc.freenode.net</a>, if the channel topic there doesn't already mention it.
 <?php endBox(); ?>
