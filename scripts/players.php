d35cbc80874bc0273e069a67073422ac09a18c95
kymara <kymara@users.sourceforge.net>
2009-08-11 21:38:26 +0000

    revert to 1.13

--- scripts/players.php
+++ scripts/players.php
@@ -32,8 +32,6 @@ class Player {
   public $age;
   /* XP of the player. It is a special attribute. */
   public $xp;
-  /* The spouse name if they are married */
-  public $spouse;
   /* adminlevel */
   public $adminlevel;
   /* Attributes the player has as a array key=>value */
@@ -43,14 +41,13 @@ class Player {
   /* Equipment the player has in slots in a array slot=>item */
   public $equipment;
   
-  function __construct($name, $sentence, $age, $level, $xp, $married, $outfit, $money, $adminlevel, $attributes, $equipment) {
+  function __construct($name, $sentence, $age, $level, $xp, $outfit, $money, $adminlevel, $attributes, $equipment) {
     $this->name=$name;
     $this->sentence=$sentence;
     $this->age=$age;
     $this->level=$level;
     $this->outfit=$outfit;
     $this->xp=$xp;
-    $this->married=$married;
     $this->attributes=$attributes;
     $this->adminlevel=$adminlevel;
     $this->money=$money;
@@ -189,7 +186,6 @@ function _getPlayers($query) {
                      $row['age'],
                      $row['level'],
                      $row['xp'],
-                     $row['married'],
                      $row['outfit'],
                      $row['money'],
                      $row['admin'],
