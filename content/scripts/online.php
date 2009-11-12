b7e64154b84e354bcabd02fb2d1dd4943dbff519
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-11-12 19:04:16 +0000

    fixed syntax error

--- content/scripts/online.php
+++ content/scripts/online.php
@@ -9,7 +9,7 @@ echo '<div style="height: 700px;">';
 foreach($players as $p) {
     echo '<div class="onlinePlayer">';
     echo '  <img src="createoutfit.php?outfit='.$p->outfit.'" alt="Player outfit"/>';
-    echo '  <a href="?id=content/scripts/character&amp;name='.htmlspecialchars(urlencode($p->name).'">';
+    echo '  <a href="?id=content/scripts/character&amp;name='.urlencode($p->name).'">';
     echo '  <div class="name">'.htmlspecialchars(utf8_encode($p->name)).'</div>';
     echo ' </a>';
     echo '</div>';
