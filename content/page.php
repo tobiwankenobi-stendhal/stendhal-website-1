b6ef6bfe52e890fe70dffe8e0b8c3f19e65e102d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2009-11-19 21:17:59 +0000

    created an interface for pages, so that pages can have a say on writing http-headers (for example to set caching-headers), the html-header (for example to include a unique title) and the content area.

---
+++ content/page.php
@@ -0,0 +1,49 @@
+<?php
+/*
+ Stendhal website - a website to manage and ease playing of Stendhal game
+ Copyright (C) 2008-2009  The Arianne Project
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * this class represents a page of the Stendhal website
+ *
+ * @author hendrik
+ */
+class Page {
+	
+	/**
+	 * this method can write additional http headers, for example for cache control.
+	 */
+	public function writeHttpHeader() {
+		// do nothing
+	}
+
+	/**
+	 * this method can write additional html headers, for example the &lt;title&gt; tag.
+	 */
+	public function writeHtmlHeader() {
+		// do nothing
+	}
+
+	/**
+	 * this methods writes the content area of the page.
+	 */
+	public function writeContent() {
+		// do nothing
+	}
+}
+$page = new Page();
+?>
