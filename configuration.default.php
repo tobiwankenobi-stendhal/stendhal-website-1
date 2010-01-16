3d079d786e0ac90b9c1000433fe19a998f6d8f58
kymara <kymara@users.sourceforge.net>
2010-01-16 17:29:52 +0000

    urlrewrite is in scripts/ not script/

--- configuration.default.php
+++ configuration.default.php
@@ -74,7 +74,7 @@ define('STENDHAL_WEB_COUNTER', true);
 
 
 # URL-Rewriting. 
-# Please see script/urlrewrite.php for the mod_rewrite rules required in your apache configuration file.
+# Please see scripts/urlrewrite.php for the mod_rewrite rules required in your apache configuration file.
 define('STENDHAL_MODE_REWRITE', false);
 
 
