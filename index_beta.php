daac83025ea9e3b37aad38ecc802018ecc5c7d30
kymara <kymara@users.sourceforge.net>
2010-01-20 21:33:02 +0000

    Use the little forum button I made

--- index_beta.php
+++ index_beta.php
@@ -160,7 +160,7 @@ require_once($page_url.'.php');
             <li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalFAQ"><img src="/images/buttons/faq_button.png" alt="">FAQ</a></li>
             <li><a href="http://stendhal.game-host.org/wiki/index.php/AskForHelp"><img src="/images/buttons/help_button.png" alt="">Ask For Help</a></li>
             <li><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=1111&amp;atid=201111"><img src="/images/buttons/faq_button.png" alt="">Support</a></li>
-            <li><a href="http://sourceforge.net/forum/forum.php?forum_id=3190"><img src="/images/buttons/faq_button.png" alt="">Forum</a></li>
+            <li><a href="http://sourceforge.net/forum/forum.php?forum_id=3190"><img src="/images/buttons/forum_button.png" alt="">Forum</a></li>
             <li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalRuleSystem"><img src="/images/buttons/rules_button.png" alt="">Rules</a></li>
           </ul>
         <?php endBox() ?>
