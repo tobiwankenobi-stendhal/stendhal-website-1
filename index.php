32c26ced4690a50cde567136c9ff861ba62b0f40
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-27 22:30:19 +0000

    replaced index.php with index_beta.php

--- index.php
+++ index.php
@@ -81,109 +81,141 @@ require_once("content/page.php");
 require_once($page_url.'.php');
 
 ?>
-<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN"
-       "http://www.w3.org/TR/html4/loose.dtd">
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
-  <head>
+	<head>
 	<link rel="stylesheet" type="text/css" href="/style.css">
 	<link rel="icon" type="image/png" href="/images/favicon.png">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<?php
-	  /*
-	   * Does another style sheet for this page exists?
-	   * Yes? Load it.
-	   */
-	  if(file_exists($page_url.'.css')) {
-	    ?>
-	    <link rel="stylesheet" type="text/css" href="/<?php echo $page_url; ?>.css">
-	    <?php
-	  }
-	  $page->writeHtmlHeader();
-	  if (!defined("STENDHAL_WANT_ROBOTS") || !constant("STENDHAL_WANT_ROBOTS")) {
-	    echo '<meta name="robots" content="noindex">'."\n";
-	  }
+		/*
+		 * Does another style sheet for this page exists?
+		 * Yes? Load it.
+		 */
+		if(file_exists($page_url.'.css')) {
+			?>
+			<link rel="stylesheet" type="text/css" href="/<?php echo $page_url; ?>.css">
+			<?php
+		}
+		$page->writeHtmlHeader();
+		if (!defined("STENDHAL_WANT_ROBOTS") || !constant("STENDHAL_WANT_ROBOTS")) {
+			echo '<meta name="robots" content="noindex">'."\n";
+		}
 	?>
-  </head>
+	</head>
 
-  <body>
-      <div id="container">
-      <div id="header">
-        <a href="/"><img style="border: 0;" src="/images/logo.gif" alt="Stendhal Logo"></a>
-      </div>
-      
-      <div id="account">
-        <?php displayLogin(); ?>
-      </div>
-      
-      <div id="topMenu">
-        <ul>
-          <li id="manual_button"><a href="http://stendhal.game-host.org/wiki/index.php/StendhalManual"><img src="/images/menu/manual.png" alt="Manual"></a></li>
-          <li id="support_button"><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=1111&amp;atid=201111"><img src="/images/menu/support.png" alt="Support"></a></li>
-          <li id="forum_button"><a href="http://sourceforge.net/forum/forum.php?forum_id=3190"><img src="/images/menu/forum.png" alt="Forum"></a></li>
-          <li id="downloads_button"><a href="http://sourceforge.net/project/platformdownload.php?group_id=1111&amp;sel_platform=410"><img src="/images/menu/download.png" alt="Downloads"></a></li>
-          <?php echo '<li id="hof_button"><a href="'.rewriteURL('/world/hall-of-fame.html').'"><img src="/images/menu/halloffame.png" alt="Hall of Fame"></a></li>'."\n"; ?>
-        </ul>
-      </div>
-      
-      <div id="leftArea">
-        <?php 
-          startBox('Screenshot');
-          /*
-           * Return the latest screenshot added to the webpage.
-           */
-          $screen=getLatestScreenshot();
-          
-          ?>
-      <a href="<? echo rewriteURL('/images/image/'.htmlspecialchars($screen->url)); ?>">
-        <?php $screen->showThumbnail(); ?>
-          </a>
-          <?php
-          endBox() 
-        ?>
-        
-        <?php startBox('Movie'); ?>
-          <a href="/wiki/index.php/Stendhal_Videos"><img src="/images/video.jpeg" width="99%" style="border: 0;"></a>
-        <?php endBox() ?>
+<body>
+<div id="container">
+	<div id="header">
+		<a href="/"><img style="border: 0;" src="/images/logo.gif" alt="Stendhal Logo"></a>
+	</div>
 
-        <?php 
-          startBox('Events');
-          /*
-           * Return the two latest events added to the website.
-           * Even if they have already happened.
-           */
-          $events=getEvents();
-          foreach($events as $i) {
-            $i->show();
-          }
-          endBox(); 
-        ?>
-        
-        <?php startBox('Game System'); ?>
-          <ul id="menu">
-            <li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalFAQ"><img src="/images/buttons/faq_button.png" alt="">FAQ</a></li>
-            <li><a href="http://stendhal.game-host.org/wiki/index.php/AskForHelp"><img src="/images/buttons/help_button.png" alt="">Help</a></li>
-            <li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalRuleSystem"><img src="/images/buttons/rules_button.png" alt="">Rules</a></li>
-            <?php 
-            echo '<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalAtlas"><img src="/images/buttons/atlas_button.png" alt="">Atlas</a></li>'."\n";
-            echo '<li><a href="'.rewriteURL('/npc/').'"><img src="/images/buttons/npcs_button.png" alt="">NPCs</a></li>'."\n";
-            echo '<li><a href="'.rewriteURL('/creature/').'"><img src="/images/buttons/creatures_button.png" alt="">Creatures</a></li>'."\n";
-            echo '<li><a href="'.rewriteURL('/item/').'"><img src="/images/buttons/items_button.png" alt="">Items</a></li>'."\n";
-            ?>
-            <li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalQuest"><img src="/images/buttons/quests_button.png" alt="">Quests</a></li>
+	<div id="account">
+		<?php displayLogin(); ?>
+	</div>
+
+	<div id="topMenu"></div>
+
+	<div id="leftArea">
+		<?php 
+		startBox('Screenshot');
+		// Return the latest screenshot added to the webpage.
+		$screen=getLatestScreenshot();
+		?>
+		<a href="<? echo rewriteURL('/images/image/'.htmlspecialchars($screen->url)); ?>">
+			<?php $screen->showThumbnail(); ?>
+		</a>
+		<?php endBox() ?>
+
+		<?php startBox('Movie'); ?>
+			<a href="/wiki/index.php/Stendhal_Videos"><img src="/images/video.jpeg" width="99%" style="border: 0;"></a>
+		<?php endBox() ?>
+
+		<?php startBox('Game System'); ?>
+		<ul id="gamemenu" class="menu">
+			<?php 
+			echo '<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalAtlas"><img src="/images/buttons/atlas_button.png" alt="">Atlas</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/npc/').'"><img src="/images/buttons/npcs_button.png" alt="">NPCs</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/creature/').'"><img src="/images/buttons/creatures_button.png" alt="">Creatures</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/item/').'"><img src="/images/buttons/items_button.png" alt="">Items</a></li>'."\n";
+			?>
+			<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalQuest"><img src="/images/buttons/quests_button.png" alt="">Quests</a></li>
 			<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalHistory"><img src="/images/buttons/history_button.png" alt="">History</a></li>
-           </ul>
-        <?php endBox(); ?>
-      </div>
-      
-      <div id="rightArea">
-        <a href="http://arianne.sourceforge.net/jws/stendhal.jnlp"><div id="playArea"></div></a>
-        <a href="http://downloads.sourceforge.net/arianne/stendhal-FULL-<?php echo STENDHAL_VERSION; ?>.zip"><div id="downloadArea"></div></a>
+		</ul>
+		<?php endBox(); ?>
 
-        <?php 
+		<?php startBox('Help'); ?>
+		<ul id="helpmenu" class="menu">
+			<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalManual"><img src="/images/buttons/manual_button.png" alt="">Manual</a></li>
+			<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalFAQ"><img src="/images/buttons/faq_button.png" alt="">FAQ</a></li>
+			<li><a href="http://stendhal.game-host.org/wiki/index.php/AskForHelp"><img src="/images/buttons/help_button.png" alt="">Ask For Help</a></li>
+			<li><a href="<?php echo rewriteURL('/development/chat.html');?>"><img src="/images/buttons/c_chat_button.png" alt="">Chat</a></li>
+			<li><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=1111&amp;atid=201111"><img src="/images/buttons/faq_button.png" alt="">Support-Ticket</a></li>
+			<li><a href="http://sourceforge.net/forum/forum.php?forum_id=3190"><img src="/images/buttons/forum_button.png" alt="">Forum</a></li>
+			<li><a href="http://stendhal.game-host.org/wiki/index.php/StendhalRuleSystem"><img src="/images/buttons/rules_button.png" alt="">Rules</a></li>
+		</ul>
+		<?php endBox() ?>
+
+	</div>
+
+	<div id="rightArea">
+		<a href="http://arianne.sourceforge.net/jws/stendhal.jnlp"><div id="playArea"></div></a>
+		<a href="http://downloads.sourceforge.net/arianne/stendhal-FULL-<?php echo STENDHAL_VERSION; ?>.zip"><div id="downloadArea"></div></a>
+
+
+		<?php 
+		startBox('Best Player');
+		$player=getBestPlayer(REMOVE_ADMINS_AND_POSTMAN);
+		if($player!=NULL) {
+			$player->show();
+		} else {
+			?>
+			<div class="small_notice">
+				No players registered.
+			</div>
+			<?php
+		}
+		endBox(); ?>
+
+		<?php startBox('Players'); ?>
+		<ul class="menu">
+			<li style="white-space: nowrap"><a href="<?php echo rewriteURL('/world/online.html');?>"><img src="/images/buttons/faq_button.png" alt=""><b style="color: #00A; size: +1"><?php echo getAmountOfPlayersOnline(); ?></b>&nbsp;Players&nbsp;Online</a></li>
+			<li><a href="<?php echo rewriteURL('/world/hall-of-fame.html')?>"><img src="/images/buttons/quests_button.png" alt="">Hall Of Fame</a></li>
+            <li><a href="<?php echo rewriteURL('/world/kill-stats.html')?>"><img src="/images/buttons/items_button.png" alt="">Kill stats</a></li>
+		</ul>
+		<form method="get" action="/" accept-charset="iso-8859-1">
+			<input type="hidden" name="id" value="content/scripts/character">
+			<input type="text" name="name" maxlength="30" style="width:9.8em">
+			<input type="submit" name="search" value="Search">
+		</form>
+		<?php endBox(); ?>
+
+
+		<?php 
+		/*
+		 * Show the admin menu only if player is really an admin.
+		 * Admins are designed using the stendhal standard way.
+		 */
+		if(getAdminLevel()>=100) {
+			startBox('Administration'); ?>
+			<ul id="adminmenu" class="menu">
+				<?php 
+				if(getAdminLevel()>=400) { ?>
+					<li><a href="/?id=content/admin/news"><img src="/images/buttons/news_button.png" alt="">News</a></li>
+					<li><a href="/?id=content/admin/events"><img src="/images/buttons/events_button.png" alt="">Events</a></li>
+					<li><a href="/?id=content/admin/screenshots"><img src="/images/buttons/screenshots_button.png" alt="">Screenshots</a></li>
+					<li><a href="/?id=content/admin/movies"><img src="/images/buttons/movies_button.png" alt="">Movies</a></li>
+				<?php } ?>
+				<li><a href="/?id=content/admin/logs"><img src="/images/buttons/c_chat_button.png" alt="">Support Logs</a></li>
+				<li><a href="/?id=content/admin/playerhistory"><img src="/images/buttons/playerhistory_button.png" alt="">Player History</a></li>
+			</ul>
+			<?php endBox();
+		}?>
+
+<?php 
+/*
           startBox('Server stats');
-          /*
-           * Load server stats about online players, bytes send/recv and other from database.
-           */
+          // Load server stats about online players, bytes send/recv and other from database.
           $stats=getServerStats();
           if(!isset($stats) || !$stats->isOnline()) {
             ?>
@@ -203,144 +235,78 @@ require_once($page_url.'.php');
           <?php
           }
           endBox(); 
-        ?>
+*/        ?>
 
-        <?php 
-          startBox('Best Player');
-          /*
-           * Get the best player related to the amount of XP earn against its age.
-           */
-          $player=getBestPlayer(REMOVE_ADMINS_AND_POSTMAN);
-          if($player!=NULL) {
-            $player->show();
-          } else {
-          	?>
-          	<div class="small_notice">
-          		No players registered.
-          	</div>
-          	<?          }
-          endBox(); 
-        ?>
-        
-        <?php 
-          startBox('Who is...');
-          ?>
-          <form method="get" action="/" accept-charset="iso-8859-1">
-            <input type="hidden" name="id" value="content/scripts/character">
-            <input type="text" name="name" maxlength="32">
-            <input type="submit" name="search" value="Search">
-          </form>
-          <?php
-          endBox(); 
-        ?>
-        
-        <?php 
-        /*
-         * Show the admin menu only if player is really an admin.
-         * Admins are designed using the stendhal standard way.
-         */
-        if(getAdminLevel()>=100) {
-          startBox('Administration'); ?>
-          <ul id="menu">
-	<?php 
-		if(getAdminLevel()>=400) { ?>
-            <li><a href="/?id=content/admin/news"><img src="/images/buttons/news_button.png" alt="">News</a></li>
-            <li><a href="/?id=content/admin/events"><img src="/images/buttons/events_button.png" alt="">Events</a></li>
-            <li><a href="/?id=content/admin/screenshots"><img src="/images/buttons/screenshots_button.png" alt="">Screenshots</a></li>
-            <li><a href="/?id=content/admin/movies"><img src="/images/buttons/movies_button.png" alt="">Movies</a></li>
-<?php 	} ?>
-          	<li><a href="/?id=content/admin/logs"><img src="/images/buttons/c_chat_button.png" alt="">Support Logs</a></li>
-			<li><a href="/?id=content/admin/playerhistory"><img src="/images/buttons/playerhistory_button.png" alt="">Player History</a></li>
-          </ul>
-        <?php 
-          endBox();
-          }       
-        ?>
+		<?php startBox('Contribute'); ?>
+		<ul id="contribmenu" class="menu">
+			<?php
+			echo '<li><a href="'.rewriteURL('/development/chat.html').'"><img src="/images/buttons/c_chat_button.png" alt="">Chat</a></li>'."\n";
+			echo '<li><a href="/wiki/index.php/Stendhal"><img src="/images/buttons/c_wiki_button.png" alt="">Wiki</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/development/bug.html').'"><img src="/images/buttons/c_bug_button.png" alt="">Report Bug</a></li>'."\n";
+			echo '<li><a href="/wiki/index.php/Stendhal_Quest_Contribution"><img src="/images/buttons/quests_button.png" alt="">Quests</a></li>'."\n";
+			echo '<li><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=1111&amp;atid=301111"><img src="/images/buttons/help_button.png" alt="">Submit Patch</a></li>'."\n";
+			echo '<li><a href="http://xplanner.homelinux.net"><img src="/images/buttons/test_button.png" alt="">Testing</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/development/cvslog.html').'"><img src="/images/buttons/history_button.png" alt="">CVS/Changes</a></li>'."\n";
+			echo '<li><a href="http://sf.net/projects/arianne/files/stendhal"><img src="/images/buttons/download_button.png" alt="">All Downloads</a></li>'."\n";
+			echo '<li><a href="'.rewriteURL('/development').'"><img src="/images/buttons/rpsystem_button.png" alt="">Development</a></li>'."\n";
+			?>
+		</ul>
+		<?php endBox(); ?>
+	</div>
 
-       <?php startBox('Contribute'); ?>
-        <ul  id="menu">
-          <?php
-		  echo '<li><a href="'.rewriteURL('/development/chat.html').'"><img src="/images/buttons/c_chat_button.png" alt="">Chat</a></li>'."\n";
-          echo '<li><a href="/wiki/index.php/Stendhal"><img src="/images/buttons/c_wiki_button.png" alt="">Wiki</a></li>'."\n";
-          echo '<li><a href="'.rewriteURL('/development/bug.html').'"><img src="/images/buttons/c_bug_button.png" alt="">Report Bug</a></li>'."\n";
-          echo '<li><a href="/wiki/index.php/Stendhal_Quest_Contribution"><img src="/images/buttons/quests_button.png" alt="">Quests</a></li>'."\n";
-          echo '<li><a href="http://sourceforge.net/tracker/?func=add&amp;group_id=1111&amp;atid=301111"><img src="/images/buttons/help_button.png" alt="">Submit Patch</a></li>'."\n";
-	      echo '<li><a href="http://xplanner.homelinux.net"><img src="/images/buttons/test_button.png" alt="">Testing</a></li>'."\n";
-		  echo '<li><a href="'.rewriteURL('/development/cvslog.html').'"><img src="/images/buttons/history_button.png" alt="">CVS/Changes</a></li>'."\n";
-		  echo '<li><a href="'.rewriteURL('/development').'"><img src="/images/buttons/rpsystem_button.png" alt="">Development</a></li>'."\n";
-		   ?>
-         </ul>          
-        <?php endBox(); ?>
-      </div>
-      
-      <div id="contentArea">
-        <?php
-        $cache=new Cache(array(
-            'login',
-            '/online',
-            '/admin',
-          ));
-        
-        $isCached=false;
-        
-        if(STENDHAL_CACHE_ENABLED) {
-          $isCached=$cache->start($page_url);
-        }
-        
-        if(!$isCached) {
-          /*
-           * The central area of the website.
-           * We append .php so that we avoid easy hacks on this.
-           */ 
-           $page->writeContent();
-        } else {
-		  ?>
-		  <div class="notice">Using a cached webpage.</div>
-		  <?php        	
-        }
-        $cache->end();          
-        ?>
-      </div>
-      
-      <div id="footerArea">
-        <?php
-        /*
-         * Compute how much time we took to render the page.
-         */
-        $mtime = explode(' ', microtime());
-        $totaltime = $mtime[0] + $mtime[1] - $starttime;
-        printf(' (Page generated in %.3f seconds.)', $totaltime);
-        ?>
-        <span class="copyright">&copy; 1999-2010 <a href="http://arianne.sourceforge.net">Arianne RPG</a></span>
-        <span><a href="http://sourceforge.net/projects/arianne"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=1111&amp;type=15" width="150" height="40" border="0" alt="Get Arianne RPG at SourceForge.net. Fast, secure and Free Open Source software downloads" ></a></span>
-        <span><?php include('counter.php'); ?></span>
-      </div>
-      
-	   <div class="time">
- 		<?php
- 		/*
- 	     * Show the server time
- 	     */
-     	echo 'Server time: '.date('G:i');
- 		?>      
-	  </div>
- 
-      <div class="author">
-        <?php
-        /*
-         * Shows a little chara of the authors.
-         */
-        $name=array_rand($authors);
-        ?>
-        <a href="http://arianne.sourceforge.net"><img src="<?php echo rewriteURL('/images/outfit/'.urlencode($authors[$name]).'.png');?>" alt="<?php echo htmlspecialchars($name); ?>"><br><?php echo htmlspecialchars($name); ?></a>
-      </div>
-    </div>
-  </body>
+	<div id="contentArea">
+		<?php
+		$cache = new Cache(array('login', '/online', '/admin'));
+
+		$isCached=false;
+
+		if(STENDHAL_CACHE_ENABLED) {
+			$isCached = $cache->start($page_url);
+		}
+
+		if(!$isCached) {
+			// The central area of the website.
+			$page->writeContent();
+		} else {
+			?>
+			<div class="notice">Using a cached webpage.</div>
+			<?php
+		}
+		$cache->end();
+		?>
+	</div>
+
+	<div id="footerArea">
+		<?php
+		// Compute how much time we took to render the page.
+		$mtime = explode(' ', microtime());
+		$totaltime = $mtime[0] + $mtime[1] - $starttime;
+		printf(' (Page generated in %.3f seconds.)', $totaltime);
+		?>
+		<span class="copyright">&copy; 1999-2010 <a href="http://arianne.sourceforge.net">Arianne RPG</a></span>
+		<span><a href="http://sourceforge.net/projects/arianne"><img src="http://sflogo.sourceforge.net/sflogo.php?group_id=1111&amp;type=15" width="150" height="40" border="0" alt="Get Arianne RPG at SourceForge.net." ></a></span>
+		<span><?php include('counter.php'); ?></span>
+	</div>
+
+	<div class="time">
+		<?php
+		// Show the server time
+		echo 'Server time: '.date('G:i');
+		?>
+	</div>
+
+	<div class="author">
+		<?php
+		// Shows a little chara of the authors.
+		$name=array_rand($authors);
+		?>
+		<a href="http://arianne.sourceforge.net"><img src="<?php echo rewriteURL('/images/outfit/'.urlencode($authors[$name]).'.png');?>" alt="<?php echo htmlspecialchars($name); ?>"><br><?php echo htmlspecialchars($name); ?></a>
+	</div>
+</div>
+</body>
 </html>
 
 <?php
-/*
- * Close connection to databases.
- */
+// Close connection to databases.
 disconnect();
 ?>
