977180c9c79aae1717e1ab25c8db7a56dc1bc6ca
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-02-27 22:48:48 +0000

    fixed css on test website

--- index.php
+++ index.php
@@ -84,8 +84,8 @@ require_once($page_url.'.php');
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="/style.css">
-	<link rel="icon" type="image/png" href="/images/favicon.png">
+	<link rel="stylesheet" type="text/css" href="<?echo STENDHAL_FOLDER?>/style.css">
+	<link rel="icon" type="image/png" href="<?echo STENDHAL_FOLDER?>/images/favicon.png">
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<?php
 		/*
