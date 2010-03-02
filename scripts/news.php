f5544bb449636ff5381281659b065d17f71d7c7d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-02 00:52:39 +0000

    added a method to generate nice urls for news postings

--- scripts/news.php
+++ scripts/news.php
@@ -16,71 +16,118 @@
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 /**
   * A class representing a news item without comments.
   */
 class News {
-  public $id;
-  /* Title of the news item */
-  public $title;
-  /* Date in ISO format YYYY/MM/DD HH:mm */
-  public $date;
-  /* One line description of the news item. */
-  public $oneLineDescription;
-  /* Extended description of the news item that follow the one line one. */
-  public $extendedDescription;
-  /* Images of the news item */
-  public $images;
-  
-  function __construct($id, $title, $date, $shortDesc, $longDesc, $images) {
-    $this->id=$id;
-    $this->title=$title;
-    $this->date=$date;
-    $this->oneLineDescription=$shortDesc;
-    $this->extendedDescription=$longDesc;
-    $this->images=$images;
-  }
+	public $id;
 
-  function show() {
-    /* NOTE: Fill this note with the HTML code needed to draw an News item. */
-    startBox('<div class="newsDate">'.$this->date.'</div><div class="newsTitle">'.$this->title.'</div>');
-    echo '<div class="newsContent">'.$this->oneLineDescription.'</div>';
-    echo '<div class="newsContent">'.$this->extendedDescription.'</div>';
-    endBox();
-    /* END NOTE */
-   }
+	/** Title of the news item */
+	public $title;
+
+	/** Date in ISO format YYYY/MM/DD HH:mm */
+	public $date;
+
+	/** One line description of the news item. */
+	public $oneLineDescription;
+
+	/** Extended description of the news item that follow the one line one. */
+	public $extendedDescription;
+
+	/** Images of the news item */
+	public $images;
+
+	/** description for detail page */
+	public $detailedDescription;
+
+	/** active */
+	public $active;
+
+	/** name of type */
+	public $typeTitle;
+
+	/** image of type */
+	public $typeImage;
+
+	function __construct($id, $title, $date, $shortDesc, $longDesc, $images, $detailedDescription, $active, $typeTitle, $typeImage) {
+		$this->id=$id;
+		$this->title=$title;
+		$this->date=$date;
+		$this->oneLineDescription=$shortDesc;
+		$this->extendedDescription=$longDesc;
+		$this->images=$images;
+		$this->detailedDescription = $detailedDescription;
+		$this->active = $active;
+		$this->typeTitle = $typeTitle;
+		$this->typeImage = $typeImage;
+	}
+
+	function show() {
+		/* NOTE: Fill this note with the HTML code needed to draw an News item. */
+		startBox('<div class="newsDate">'.$this->date.'</div><div class="newsTitle">'.$this->title.'</div>');
+		echo '<div class="newsContent">'.$this->oneLineDescription.'</div>';
+		echo '<div class="newsContent newsTeaser">'.$this->extendedDescription.'</div>';
+		endBox();
+		/* END NOTE */
+	}
+
+	/**
+	 * gets a nice url
+	 *
+	 * @return nice url
+	 */
+	function getNiceURL() {
+		$res = strtolower($this->title.'-'.$this->id);
+		$res = preg_replace('/[ _,;.:<>|] /', ' ', $res);
+		$res = preg_replace('/[ _,;.:<>|]/', '-', $res);
+		return urlencode($res);
+	}
 };
 
 /**
   * Returns a list of news. Note: All parameters need to be SQL escaped.
   */
 function getNews($where='', $sortby='created desc', $cond='limit 3') {
-    
-	$result = mysql_query('select * from news '.$where.' order by '.$sortby.' '.$cond, getWebsiteDB());
-    $list=array();
-    
-    while($row=mysql_fetch_assoc($result)) {
-      $resultimages = mysql_query('select * from news_images where news_id="'.$row['id'].'" order by created desc', getWebsiteDB());
-      $images=array();
-      
-      while($rowimages=mysql_fetch_assoc($resultimages)) {      
-        $images[]=$rowimages['url'];
-      }
-      mysql_free_result($resultimages);
-      
-      $list[]=new News(
-                     $row['id'],
-                     $row['title'],
-                     $row['created'],
-                     $row['shortDescription'],
-                     $row['extendedDescription'],
-                     $images);
-    }
-    
-    mysql_free_result($result);
+
+	$sql = 'SELECT news.id As news_id, news.title As title, news.created As created, '
+		.'news.shortDescription As shortDescription, '
+		.'news.extendedDescription As extendedDescription, '
+		.'news.detailedDescription As detailedDescription, news.active As active, '
+		.'news_type.title As type_title, news_type.image_url As image_url ' 
+		.'FROM news LEFT JOIN news_type ON news.news_type_id=news_type.id '.$where.' order by '.$sortby.' '.$cond;
+
+	$result = mysql_query($sql, getWebsiteDB());
+	$list=array();
+
+	while($row=mysql_fetch_assoc($result)) {
+		$resultimages = mysql_query('select * from news_images where news_id="'.$row['id'].'" order by created desc', getWebsiteDB());
+		$images=array();
+
+		while($rowimages=mysql_fetch_assoc($resultimages)) {      
+			$images[]=$rowimages['url'];
+		}
+		mysql_free_result($resultimages);
+
+		$list[]=new News(
+			$row['news_id'],
+			$row['title'],
+			$row['created'],
+			$row['shortDescription'],
+			$row['extendedDescription'],
+			$images,
+			$row['detailedDescription'],
+			$row['active'],
+			$row['type_title'],
+			$row['image_url']
+		);
+	}
+
+	mysql_free_result($result);
 	
-    return $list;
-  }
+	return $list;
+}
+
 
 function addNews($title, $oneline, $body, $images, $approved=false) {
     $title=mysql_real_escape_string($title);
