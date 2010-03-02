7a39b8005736bfe417eeb3abf7e97ed7fae05edb
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-03-02 22:11:57 +0000

    readded links

--- scripts/news.php
+++ scripts/news.php
@@ -70,7 +70,7 @@ class News {
 	function show($detail=false) {
 		// link the title unless we are in detail view
 		$heading = '<div class="newsDate">'.$this->date.'</div><div class="newsTitle">';
-		if (FALSE) {
+		if (!$detail) {
 			$heading .= '<a style="newsTitle" href="'.rewriteURL('/news/'.$this->getNiceURL()).'">'.$this->title.'</a>';
 		} else {
 			$heading .= $this->title;
