660acaa25d1945c111581c6384e207099329f355
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-04-04 18:38:49 +0000

    deleted events

--- stendhal_website.sql
+++ stendhal_website.sql
@@ -21,31 +21,6 @@ create table movies (
 );
 
 
-create table events (
-  id int auto_increment not null,
-  date datetime not null,
-  type varchar(32),
-  location varchar(32),
-  shortDescription varchar(256),
-  extendedDescription text,
-  
-  created timestamp default CURRENT_TIMESTAMP,
-  
-  primary key(id)
-);
-
-insert into events values(null, "2008/06/01 18:50", "raid", "Ados", "Website reading from database", "No extended description", null);
-insert into events values(null, "2008/06/01 21:50", "raid", "Semos", "Goblins!", "No extended description", null);
-
-create table event_images (
-  id int auto_increment not null,
-  event_id int not null,
-  url varchar(256),
-  description text,
-  created timestamp default CURRENT_TIMESTAMP,
-  
-  primary key(id, event_id)
-);
 
 create table news (
   id int auto_increment not null,
