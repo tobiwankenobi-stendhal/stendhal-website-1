3cb706776736dcd71e8697dd3b32da5342395c55
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-04-05 15:11:52 +0000

    added update_count column to news table

--- stendhal_website.sql
+++ stendhal_website.sql
@@ -31,7 +31,7 @@ create table news (
   active int default 1,
   news_type_id int,
   created timestamp default CURRENT_TIMESTAMP,
-  
+  modified timestamp default CURRENT_TIMESTAMP update CURRENT_TIMESTAMP,
   primary key(id)
 );
 
