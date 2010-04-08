c4665d182c3abc71a21d3c07a4e1107b3086c00a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-04-08 22:25:59 +0000

    added translations of "game" to keywords

--- content/main.php
+++ content/main.php
@@ -20,9 +20,11 @@
 class MainPage extends Page {
 
 	public function writeHtmlHeader() {
-		echo '<title>News'.STENDHAL_TITLE.'</title>';
-		echo '<link rel="alternate" type="application/rss+xml" title="Stendhal News" href="'.rewriteURL('/rss/news.rss').'" />';
+		echo '<title>News'.STENDHAL_TITLE.'</title>'."\n";
+		echo '<link rel="alternate" type="application/rss+xml" title="Stendhal News" href="'.rewriteURL('/rss/news.rss').'" />'."\n";
+		echo '<meta name="keywords" content="Stendhal, game, gra, Spiel, juego, role, gioco, online, open, source, multiplayer, news, Arianne';
 		echo '<meta name="description" content="Stendhal is a fully fledged free open source multiplayer online adventures game (MORPG) developed using the Arianne game system.">';
+		
 	}
 
 	function writeContent() {
