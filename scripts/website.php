dca994ca53f2d5df81ca8c1066bd559e4fa3393f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-04-12 16:33:48 +0000

    removed duplicated copyright block, added Arianne Project

--- scripts/website.php
+++ scripts/website.php
@@ -2,6 +2,7 @@
 /*
  Stendhal website - a website to manage and ease playing of Stendhal game
  Copyright (C) 2008  Miguel Angel Blanch Lardin
+ Copyright (C) 2008-2010  The Arianne Project
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as published by
@@ -15,25 +16,9 @@
 
  You should have received a copy of the GNU Affero General Public License
  along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+*/
 
-/*
-    Stendhal website - a website to manage and ease playing of Stendhal game
-    Copyright (C) 2008  Miguel Angel Blanch Lardin
 
-    This program is free software: you can redistribute it and/or modify
-    it under the terms of the GNU Affero General Public License as published by
-    the Free Software Foundation, either version 3 of the License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-    GNU Affero General Public License for more details.
-
-    You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
-  */
  /*
  * This file is the PHP code that generate each of the website sections. 
  */
@@ -44,23 +29,13 @@ function startBox($title) {
   echo '<div class="box">';
   echo '<div class="boxTitle">'.$title.'</div>';
   echo '<div class="boxContent">';
-  }
+}
 
 function endBox() {
   echo '</div></div>';
-  }
-  
-function showKnownBugNotice() {
-if(STENDHAL_PLEASE_MAKE_IT_FAST){
-  ?>
-    <div class="notice">
-      The following data is not correct and that's a known bug.<br/>
-      As soon as we fix this problem you will see real data from game server.
-    </div>
-  <?php
- }
 }
-  
+
+
 include('scripts/screenshots.php');
 include('scripts/news.php');
 include('scripts/players.php');
