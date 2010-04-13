42eb56061fb74854e8916cbf07c6dbaad3376a7e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-04-13 20:25:22 +0000

    fixed html warning about empty span text for empty line at the end

--- content/game/chat.php
+++ content/game/chat.php
@@ -60,7 +60,9 @@ for ($i = 0; $i < count($lines); $i++) {
 	$line = htmlspecialchars($line);
 	$line = preg_replace('!(http|https)://(stendhalgame.org|arianne.sf.net|arianne.sourceforge.net|sourceforge.net|sf.net)(/[^ ]*)?!', '<a href="$1://$2$3">$1://$2$3</a>', $line);
 
-	echo '<span class="'.$class.'">'.$line.'</span><br>'."\n";
+	if ($line != '') {
+		echo '<span class="'.$class.'">'.$line.'</span><br>'."\n";
+	}
 }
 
 ?>
