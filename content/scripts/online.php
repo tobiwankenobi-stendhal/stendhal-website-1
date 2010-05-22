0053142daee0a46121092b11654a959d7ecfa3b5
kymara <kymara@users.sourceforge.net>
2010-05-22 11:08:14 +0000

    fixing bug [stendhal-website] "Best Player Box" changes size of letters  ID: 2978315, by not using the same css label name for the small print names online as the one always used in the best player box. also converted windows line endings and corrected grammar of message when no players are online

--- content/scripts/online.php
+++ content/scripts/online.php
@@ -12,14 +12,14 @@ $players=getOnlinePlayers();
 startBox('Online Players');
 
 if(sizeof($players)==0) {
-  echo 'There are no logged players';
+  echo 'There are no players logged in';
 }
 echo '<div style="height: 700px;">';
 foreach($players as $p) {
     echo '<div class="onlinePlayer">';
     echo '  <a href="'.rewriteURL('/character/'.surlencode($p->name).'.html').'">';
     echo '  <img src="'.rewriteURL('/images/outfit/'.surlencode($p->outfit).'.png').'" alt=""></a>';
-    echo '  <div class="name"><a href="'.rewriteURL('/character/'.surlencode($p->name).'.html').'">';
+    echo '  <div class="onlinename"><a href="'.rewriteURL('/character/'.surlencode($p->name).'.html').'">';
     echo htmlspecialchars(utf8_encode($p->name)).'</a></div>';
     echo '</div>';
 }
