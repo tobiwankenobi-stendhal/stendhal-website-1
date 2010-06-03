073d8ec695b43a7937f0c73f934ada96a7a84b32
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-03 21:52:54 +0000

    fixed redirect url on logout, which sometimes displayed that the logout failed because the user was not logged anymore (fixes #3009974)

--- content/account/logout.php
+++ content/account/logout.php
@@ -4,46 +4,35 @@ require_once('scripts/account.php');
 
 class LogoutPage extends Page {
 
+	public function writeHttpHeader() {
+		/**
+		 * Delete cookies - the time must be in the past,
+		 * so just negate what you added when creating the
+		 * cookie.
+		 */
+		if(isset($_COOKIE['cookname']) && isset($_COOKIE['cookpass'])){
+			setcookie("cookname", "", time()-60*60*24*100, "/");
+			setcookie("cookpass", "", time()-60*60*24*100, "/");
+		}
+
+		// Kill session variables
+		unset($_SESSION['username']);
+		unset($_SESSION['password']);
+		$_SESSION = array(); // reset session array
+		session_destroy();   // destroy session.
+		return true;
+	}
+
 	public function writeHtmlHeader() {
 		echo '<title>Logout'.STENDHAL_TITLE.'</title>';
 		echo '<meta name="robots" content="noindex">'."\n";
+		echo '<meta http-equiv="Refresh" content="0;url=/">';
 	}
 
 	function writeContent() {
-
-/**
- * Delete cookies - the time must be in the past,
- * so just negate what you added when creating the
- * cookie.
- */
-if(isset($_COOKIE['cookname']) && isset($_COOKIE['cookpass'])){
-	setcookie("cookname", "", time()-60*60*24*100, "/");
-	setcookie("cookpass", "", time()-60*60*24*100, "/");
-}
-
-?>
-
-<html>
-<title>Logging Out</title>
-<body>
-
-<?php
-startBox("Logout");
-if(!checkLogin()) {
-	echo "You are not currently logged in, logout failed. Back to <a href=\"?\">main</a>";
-} else {
-	/* Kill session variables */
-	unset($_SESSION['username']);
-	unset($_SESSION['password']);
-	$_SESSION = array(); // reset session array
-	session_destroy();   // destroy session.
-
-	echo "<meta http-equiv=\"Refresh\" content=\"0;url=?\">";
-	echo "<h1>Logged Out</h1>\n";
-	echo "You have successfully <b>logged out</b>.<p>Back to <a href=\"?\">main</a>";
-}
-
-endBox();
+		echo "<h1>Logged Out</h1>\n";
+		echo 'You have successfully <b>logged out</b>.<p>Back to <a href="/">start page</a>'';
+		endBox();
 	}
 }
 $page = new LogoutPage();
