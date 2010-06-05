b3dea571534e6d4da11de2fbde628ee4d8e0ab86
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-05 10:14:04 +0000

    fixed seed beeing too short

--- content/account/starter.php
+++ content/account/starter.php
@@ -73,7 +73,7 @@ class StarterPage extends Page {
 		$characters = '0123456789abcdefghijklmnopqrstuvwxyz';
 		$this->seed = '';
 		for ($i = 0; $i < 16; $i++) {
-			$this->seed .= $characters[mt_rand(0, strlen($characters))];
+			$this->seed .= $characters[mt_rand(0, strlen($characters) - 1)];
 		}
 		storeSeed($this->username, $_SERVER['REMOTE_ADDR'], $this->seed, 1);
 	}
