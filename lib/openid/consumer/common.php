345d630ad1907cabcccd56e9ca60433b08674ce5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-06 22:00:29 +0000

    adjusted search path

--- lib/openid/consumer/common.php
+++ lib/openid/consumer/common.php
@@ -1,6 +1,6 @@
 <?php
 
-$path_extra = dirname(dirname(dirname(dirname(__FILE__))));
+$path_extra = dirname(dirname(__FILE__));
 $path = ini_get('include_path');
 $path = $path_extra . PATH_SEPARATOR . $path;
 ini_set('include_path', $path);
