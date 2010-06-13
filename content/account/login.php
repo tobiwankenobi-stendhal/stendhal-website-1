287af4d1f6a3c7c0e4c19af47bc71fe84ae74ba8
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-13 10:11:23 +0000

    code format, no other changes

--- content/account/login.php
+++ content/account/login.php
@@ -10,121 +10,129 @@ class LoginPage extends Page {
 	}
 
 	function writeContent() {
+		
+		/**
+		 * Checks to see if the user has submitted his
+		 * username and password through the login form,
+		 * if so, checks authenticity in database and
+		 * creates session.
+		 */
+		if(isset($_POST['sublogin'])) {
+			$this->checkLogin();
+		} else {
+			$this->displayLoginForm();
+		}
+	}
 
-/**
- * Checks to see if the user has submitted his
- * username and password through the login form,
- * if so, checks authenticity in database and
- * creates session.
- */
-if(isset($_POST['sublogin'])){
-   /* Check that all fields were typed in */
-   if(!$_POST['user'] || !$_POST['pass']){
-     die('You didn\'t fill in a required field.');
-     }
-     
-   /* Spruce up username, check length */
-   $_POST['user'] = trim($_POST['user']);
-   if(strlen($_POST['user']) > 30){
-     startBox("Login failed");
-     echo "Sorry, the username is longer than 30 characters, please shorten it.";
-     endBox();
-     return;
-     }
 
-   /* We first check that the username is not banned. 
-    */
-   $result = confirmValidStatus($_POST['user']);
-   
-   /* Check error codes */
-   if($result == 2){
-   	 /*
-   	  * If result==1 then username doesn't exist, so we let the password check handle it.
-   	  */
-     startBox("Login failed");
-     echo "Sorry. Your account is blocked by multiple passwords failures or it has been banned.";
-     endBox();
-     return;
-     }
+	function checkLogin() {
+			/* Check that all fields were typed in */
+		if(!$_POST['user'] || !$_POST['pass']) {
+			die('You didn\'t fill in a required field.');
+		}
 
-   /* Checks that username is in database and password is correct */
-   $md5pass = strtoupper(md5($_POST['pass']));
-   $result = confirmUser($_POST['user'], $md5pass);
-   
-   if ($result === 2) {
-     /* We need to check the pre-Marauroa 2.0 passwords */
-	 $md5pass = strtoupper(md5(md5($_POST['pass'],true)));
-	 $result = confirmUser($_POST['user'], $md5pass);
-     }
+		/* Spruce up username, check length */
+		$_POST['user'] = trim($_POST['user']);
+		if(strlen($_POST['user']) > 30) {
+			startBox("Login failed");
+			echo "Sorry, the username is longer than 30 characters, please shorten it.";
+			endBox();
+			return;
+		}
 
-   /* Here we log the login attempt, with username, IP and whether failed or successful */
-   logUserLogin($_POST['user'], $_SERVER['REMOTE_ADDR'], $result == 0);
+		/* We first check that the username is not banned. */
+		$result = confirmValidStatus($_POST['user']);
+
+		/* Check error codes */
+		if($result == 2) {
+			/* If result==1 then username doesn't exist, so we let the password check handle it. */
+			startBox("Login failed");
+			echo "Sorry. Your account is blocked by multiple passwords failures or it has been banned.";
+			endBox();
+			return;
+		}
+
+		/* Checks that username is in database and password is correct */
+		$md5pass = strtoupper(md5($_POST['pass']));
+		$result = confirmUser($_POST['user'], $md5pass);
+
+		if ($result === 2) {
+			/* We need to check the pre-Marauroa 2.0 passwords */
+			$md5pass = strtoupper(md5(md5($_POST['pass'],true)));
+			$result = confirmUser($_POST['user'], $md5pass);
+		}
+
+		/* Here we log the login attempt, with username, IP and whether failed or successful */
+		logUserLogin($_POST['user'], $_SERVER['REMOTE_ADDR'], $result == 0);
+
+		/* Check error codes */
+		if($result != 0){
+			startBox("Login failed");
+			echo "Sorry. You mispelled either username or password.<br>Make sure you have an account at Stendhal.";
+			endBox();
+			return;
+		}
+
+		/* Username and password correct, register session variables */
+		$_POST['user'] = stripslashes($_POST['user']);
+		$_SESSION['username'] = $_POST['user'];
+		$_SESSION['password'] = $md5pass;
 	
-   /* Check error codes */
-   if($result != 0){
-     startBox("Login failed");
-     echo "Sorry. You mispelled either username or password.<br>Make sure you have an account at Stendhal.";
-     endBox();
-     return;
-     }
+		/**
+		 * This is the cool part: the user has requested that we remember that
+		 * he's logged in, so we set two cookies. One to hold his username,
+		 * and one to hold his md5 encrypted password. We set them both to
+		 * expire in 100 days. Now, next time he comes to our site, we will
+		 * log him in automatically.
+		 */
+		if(isset($_POST['remember'])){
+			setcookie("cookname", $_SESSION['username'], time()+60*60*24*100, "/");
+			setcookie("cookpass", $_SESSION['password'], time()+60*60*24*100, "/");
+		}
 
-   /* Username and password correct, register session variables */
-   $_POST['user'] = stripslashes($_POST['user']);
-   $_SESSION['username'] = $_POST['user'];
-   $_SESSION['password'] = $md5pass;
+		$url = "/";
+	// TODO: find a better way to whitelist parameters without hardcoding them.
+		if ($_POST['url'] == "content/account/meeting") {
+			$url = "/index.php?id=content/account/meeting";
+		}
+		echo "<meta http-equiv=\"Refresh\" content=\"1;url=".$url."\">";
+		startBox("Login");
+		echo '<h1>Login correct.</h1> Moving to main page.';
+		endBox();
+	}
 
-   /**
-    * This is the cool part: the user has requested that we remember that
-    * he's logged in, so we set two cookies. One to hold his username,
-    * and one to hold his md5 encrypted password. We set them both to
-    * expire in 100 days. Now, next time he comes to our site, we will
-    * log him in automatically.
-    */
-   if(isset($_POST['remember'])){
-      setcookie("cookname", $_SESSION['username'], time()+60*60*24*100, "/");
-      setcookie("cookpass", $_SESSION['password'], time()+60*60*24*100, "/");
-   }
-      
-   $url = "/";
-// TODO: find a better way to whitelist parameters without hardcoding them.
-   if ($_POST['url'] == "content/account/meeting") {
-     $url = "/index.php?id=content/account/meeting";
-   }
-   echo "<meta http-equiv=\"Refresh\" content=\"1;url=".$url."\">";
-   startBox("Login");
-     echo '<h1>Login correct.</h1> Moving to main page.';
-   endBox();
-} else {
-startBox("Login");
-?>
 
-<div class="bubble">
-  Remember not to disclose your username or password to anyone, not even friends or administrators.<br>
-  Check that this webpage URL matchs your game server name.
-</div>
-<form action="" method="post">
-<table>
-  <tr><td>Username:</td><td><input type="text" name="user" maxlength="30"></td></tr>
-  <tr><td>Password:</td><td><input type="password" name="pass" maxlength="30"></td></tr>
-  <tr><td colspan="2" align="left"><input type="checkbox" name="remember">
-  <font size="2">Remember me next time</font></td></tr>
-  <tr><td colspan="2" align="right"><input type="submit" name="sublogin" value="Login"></td></tr>
-</table>
-<?php
-// TODO: merge _GET and _POST
-if (isset($_GET['url'])) {
-	echo '<input type="hidden" name="url" value="'.htmlspecialchars($_GET['url']).'">';
-}
-if (isset($_POST['url'])) {
-	echo '<input type="hidden" name="url" value="'.htmlspecialchars($_POST['url']).'">';
-}
-?>
-</form>
+	function displayLoginForm() {
+		startBox("Login");
+	?>
 
-<?php
-endBox();
+		<div class="bubble">
+			Remember not to disclose your username or password to anyone, not even friends or administrators.<br>
+			Check that this webpage URL matchs your game server name.
+		</div>
+		<form action="" method="post">
+			<table>
+				<tr><td>Username:</td><td><input type="text" name="user" maxlength="30"></td></tr>
+				<tr><td>Password:</td><td><input type="password" name="pass" maxlength="30"></td></tr>
+				<tr><td colspan="2" align="left"><input type="checkbox" name="remember">
+				<font size="2">Remember me next time</font></td></tr>
+				<tr><td colspan="2" align="right"><input type="submit" name="sublogin" value="Login"></td></tr>
+			</table>
 
-}
+			<?php
+			// TODO: merge _GET and _POST
+			if (isset($_GET['url'])) {
+				echo '<input type="hidden" name="url" value="'.htmlspecialchars($_GET['url']).'">';
+			}
+			if (isset($_POST['url'])) {
+				echo '<input type="hidden" name="url" value="'.htmlspecialchars($_POST['url']).'">';
+			}
+			?>
+		</form>
+
+		<?php
+		endBox();
+
 	}
 }
 $page = new LoginPage();
