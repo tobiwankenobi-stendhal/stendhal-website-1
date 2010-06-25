7ef06a7a63ff04e7238c27aa6d809a1ff4ce2f25
kymara <kymara@users.sourceforge.net>
2010-06-25 19:09:45 +0000

    fixed order of chat channels also in the description part

--- content/game/chat.php
+++ content/game/chat.php
@@ -12,14 +12,14 @@ You can get an IRC client and connect to:
 
 <p><b>irc.freenode.net</b> (see <a href="http://freenode.net">http://freenode.net</a> for more information) then</p>
 <ul>
-<li><b>#arianne-chat</b> which is for friendly chatter </li>
-<li> <b>#arianne</b> which is for technical discussion, getting help and planning.</li>
+<li><b>#arianne</b> which is for technical discussion, ideas, bugs, getting help and planning.</li>
+<li><b>#arianne-chat</b> which is for off topic friendly chat</li>
 </ul>
 
 <p>Alternatively, you can simply use freenode's webchat service, below. Feel free to change the nick to for example, your player name.</p>
 <ul> 
 <li><a href="http://webchat.freenode.net/?channels=arianne">#arianne</a> (for ideas, contributions and support)</li>
-<li><a href="http://webchat.freenode.net/?channels=arianne-chat">#arianne-chat</a> (for chitchat not related to Arianne/Stendhal)</li>
+<li><a href="http://webchat.freenode.net/?channels=arianne-chat">#arianne-chat</a> (for off topic chat not related to Arianne/Stendhal)</li>
 </ul>
 If you are new to IRC it is well worth reading this <a href="http://www.irchelp.org/irchelp/new2irc.html">short guide</a> before you join. In particular the section on talking, and entering commands, and the section 'Some advice' may be helpful.
 <?php endBox(); ?>
