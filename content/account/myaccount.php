7774ee299ac029494ef7bda9e634196514745a37
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-27 10:01:28 +0000

    new page listing all the things you can do when logged in (should be side navigation bar box later)

---
+++ content/account/myaccount.php
@@ -0,0 +1,27 @@
+<?php
+
+class MyAccountPage extends Page {
+
+	public function writeHtmlHeader() {
+		echo '<meta name="robots" content="noindex">'."\n";
+		echo '<title>My Account'.STENDHAL_TITLE.'</title>';
+	}
+
+	function writeContent() {
+
+startBox("My Account"); ?>
+	<p>You are logged in as <b><?php echo htmlspecialchars($_SESSION['username']);?></b>.</p>
+<ul id="dmenu" >
+	<?php 
+		echo '<li><a href="'.rewriteURL('/account/change-password.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Change password</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/history.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Login history</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/merge.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Merge Accounts</a> - (<a href="https://stendhalgame.org/wiki/Stendhal_Account_Merging">Help</a>)</li>';
+		echo '<li><a href="'.rewriteURL('/account/logout.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Logout</a></li>';
+	?>
+</ul>
+<?php endBox();
+	}
+}
+$page = new MyAccountPage();
+?>
+
