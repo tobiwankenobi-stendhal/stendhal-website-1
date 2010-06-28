b88579d7b48b6c1cba9d3b12e0fe282520d1608d
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-28 20:24:24 +0000

    main include file

---
+++ scripts/parauroa/parauroa.php
@@ -0,0 +1,9 @@
+<?php
+
+define('PARAUROA_NETWORK_PROTOCOL_VERSION', 32);
+define('PARAUROA_CLIENTID_INVALID', -1);
+
+require_once ('deserializer.php');
+require_once ('serializer.php');
+require_once ('message-type.php');
+require_once ('message.php');
