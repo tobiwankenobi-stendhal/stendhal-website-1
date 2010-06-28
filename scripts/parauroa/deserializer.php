f0cdb6431c07c7095e04aa81cde0ee29dda512b0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-06-28 20:24:05 +0000

    started to work on serialization

---
+++ scripts/parauroa/deserializer.php
@@ -0,0 +1,18 @@
+<?php
+
+class ParauroaSerializer {
+	
+	private $data;
+
+	public function __construct($data) {
+		$this->data = $data;
+	}
+	
+	public function readByte($byte) {
+		return 0; // TODO
+	}
+
+	public function readerInt($int) {
+		return 0; // TODO
+	}
+}
---
+++ scripts/parauroa/serializer.php
@@ -0,0 +1,18 @@
+<?php
+
+class ParauroaSerializer {
+	
+	private $data;
+	
+	public function writeByte($byte) {
+		$data = $data . chr($byte);
+	}
+
+	public function writeInt($int) {
+		$data = $data . pack($int, "N");
+	}
+	
+	public function getData() {
+		return $data;
+	}
+}
