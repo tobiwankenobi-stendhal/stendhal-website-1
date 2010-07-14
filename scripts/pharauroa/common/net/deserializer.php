ebe513b476c8210b68321a22489e2d259b8568a0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-14 20:23:21 +0000

    added stubs

--- scripts/pharauroa/common/net/deserializer.php
+++ scripts/pharauroa/common/net/deserializer.php
@@ -5,6 +5,10 @@ class PharauroaDeserializer {
 	private $data;
 	private $protocolVersion = PHARAUROA_NETWORK_PROTOCOL_VERSION;
 
+	public function __constructor($data) {
+		$this->data = $data;
+	}
+
 	public function setProtocolVersion($protocolVersion) {
 		$this->$protocolVersion = min($protocolVersion, PHARAUROA_NETWORK_PROTOCOL_VERSION);
 	}
@@ -17,11 +21,24 @@ class PharauroaDeserializer {
 		$this->data = $data;
 	}
 	
-	public function readByte($byte) {
+	public function readByte() {
+		// TODO opposite of: $this->data = $this->data . chr($byte);
 		return 0; // TODO
 	}
 
-	public function readerInt($int) {
-		return 0; // TODO
+	public function readerInt() {
+		// TODO opposite of: $this->data = $this->data . pack("I", $int);
+		return 0; 
+	}
+
+	
+	public function readString() {
+		// TODO opposite of: $this->writeInt(strlen($string)); $this->data = $this->data . $string;
+		return "";
+	}
+
+	public function write256LongString($string) {
+		// TODO opposite of: $this->writeByte(strlen($string)); $this->data = $this->data . $string;
+		return "";
 	}
 }
