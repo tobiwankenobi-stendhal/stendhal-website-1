5120901bf5d7d2ba7254c5ece73bf81ccabe6523
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-15 06:33:05 +0000

    implemented reading methods (patch by dwidge)

--- scripts/pharauroa/common/net/deserializer.php
+++ scripts/pharauroa/common/net/deserializer.php
@@ -5,7 +5,8 @@ class PharauroaDeserializer {
 	private $data;
 	private $protocolVersion = PHARAUROA_NETWORK_PROTOCOL_VERSION;
 
-	public function __constructor($data) {
+	
+	public function __construct($data) {
 		$this->data = $data;
 	}
 
@@ -16,29 +17,35 @@ class PharauroaDeserializer {
 	public function getProtocolVersion() {
 		return $this->protocolVersion;
 	}
-
-	public function __construct($data) {
-		$this->data = $data;
-	}
-	
 	public function readByte() {
 		// TODO opposite of: $this->data = $this->data . chr($byte);
-		return 0; // TODO
+		$output = ord($this->data[0]);
+		$this->data = substr($this->data, 1);
+		return $output; // TODO
 	}
 
-	public function readerInt() {
+	public function readInt() {
+	/// I recommend you use "V" - unsigned long (always 32 bit, little endian byte order) - to keep endian ordering and size accross network
 		// TODO opposite of: $this->data = $this->data . pack("I", $int);
-		return 0; 
+		$output = unpack("V", $this->data);
+		$this->data = substr($this->data, 4);
+		return $output[0]; 
 	}
 
 	
 	public function readString() {
 		// TODO opposite of: $this->writeInt(strlen($string)); $this->data = $this->data . $string;
-		return "";
+		$length = $this->readInt();
+		$output = substr($this->data, 0, $length);
+		$this->data = substr($this->data, $length);
+		return $output;
 	}
 
 	public function write256LongString($string) {
 		// TODO opposite of: $this->writeByte(strlen($string)); $this->data = $this->data . $string;
-		return "";
+		$length = $this->readByte();
+		$output = substr($this->data, 0, $length);
+		$this->data = substr($this->data, $length);
+		return $output;
 	}
 }
