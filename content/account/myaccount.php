161811ab208902f87a92f4aa35fb1aa910f74c24
kymara <kymara@users.sourceforge.net>
2010-07-16 22:29:33 +0000

    Use new account navigation images

--- content/account/myaccount.php
+++ content/account/myaccount.php
@@ -13,10 +13,10 @@ startBox("My Account"); ?>
 	<p>You are logged in as <b><?php echo htmlspecialchars($_SESSION['username']);?></b>.</p>
 <ul id="dmenu" >
 	<?php 
-		echo '<li><a href="'.rewriteURL('/account/change-password.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Change password</a></li>';
-		echo '<li><a href="'.rewriteURL('/account/history.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Login history</a></li>';
-		echo '<li><a href="'.rewriteURL('/account/merge.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Merge Accounts</a> - (<a href="https://stendhalgame.org/wiki/Stendhal_Account_Merging">Help</a>)</li>';
-		echo '<li><a href="'.rewriteURL('/account/logout.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Logout</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/change-password.html').'"><img src="/images/buttons/password_button.png" alt=" "> Change password</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/history.html').'"><img src="/images/buttons/history_button.png" alt=" "> Login history</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/merge.html').'"><img src="/images/buttons/players_button.png" alt=" "> Merge Accounts</a> - (<a href="https://stendhalgame.org/wiki/Stendhal_Account_Merging">Help</a>)</li>';
+		echo '<li><a href="'.rewriteURL('/account/logout.html').'"><img src="/images/buttons/logout_button.png" alt=" "> Logout</a></li>';
 	?>
 </ul>
 <?php endBox();
