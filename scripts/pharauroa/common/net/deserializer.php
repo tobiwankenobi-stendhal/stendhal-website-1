b38305d3913b5d39e42ff3e1fdc536b9e863df6a
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-17 21:03:53 +0000

    removed todos

--- scripts/pharauroa/common/net/deserializer.php
+++ scripts/pharauroa/common/net/deserializer.php
@@ -10,39 +10,64 @@ class PharauroaDeserializer {
 		$this->data = $data;
 	}
 
-	// Version
+	/**
+	 * sets the protocol version
+	 *
+	 * @param $protocolVersion version of protocol
+	 */
 	public function setProtocolVersion($protocolVersion) {
 		$this->$protocolVersion = min($protocolVersion, PHARAUROA_NETWORK_PROTOCOL_VERSION);
 	}
+
+	/**
+	 * gets the protocol version
+	 *
+	 * @return $protocolVersion version of protocol
+	 */
 	public function getProtocolVersion() {
 		return $this->protocolVersion;
 	}
 
-	// Numeric
+	/**
+	 * This method reads a byte
+	 *
+	 * @return the byte 
+	 */
 	public function readByte() {
-		// TODO opposite of: $this->data = $this->data . chr($byte);
 		$output = ord($this->data[0]);
 		$this->data = substr($this->data, 1);
-		return $output; // TODO
+		return $output;
 	}
+
+	/**
+	 * This method reads an integer
+	 *
+	 * @return the integer
+	 */
 	public function readInt() {
-		// TODO opposite of: $this->data = $this->data . pack("I", $int);
 		$output = unpack("I", $this->data);
 		$this->data = substr($this->data, 4);
 		return $output[1]; 
 	}
 
-	// String
-	public function readString() {
-		// TODO opposite of: $this->writeInt(strlen($string)); $this->data = $this->data . $string;
+	/**
+	 * This method reads a long string
+	 *
+	 * @return the string
+	 */
+		public function readString() {
 		$length = $this->readInt();
 		$output = substr($this->data, 0, $length);
 		$this->data = substr($this->data, $length);
 		return $output;
 	}
 
+	/**
+	 * This method reads a short string
+	 *
+	 * @return the string
+	 */
 	public function read255LongString() {
-		// TODO opposite of: $this->writeByte(strlen($string)); $this->data = $this->data . $string;
 		$length = $this->readByte();
 		$output = substr($this->data, 0, $length);
 		$this->data = substr($this->data, $length);
