6017564bc1dca7b9bc56c169696ae36e763c4be2
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-31 15:02:28 +0000

    create account

--- scripts/pharauroa/client/clientframework.php
+++ scripts/pharauroa/client/clientframework.php
@@ -13,5 +13,30 @@ class PharauroaClientFramework {
 	function __construct($credentials) {
 		$this->credentials = $credentials;
 	}
+
+	/**
+	 * creates a new account
+	 */
+	function createAccount($username, $password, $email) {
+		// Create message
+		$message = new PharauroaMessageP2SCreateAccount();
+		$message->init($username, $password, $email);
+
+		// Serialize message
+		$serializer = new PharauroaSerializer();
+		$message->writeObject($serializer);
+		$data = $serializer->getData();
+
+		// connect and write message
+		$sock = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+		socket_connect($sock, "127.0.0.1", 32160);
+		socket_write($sock, $data, strlen($data)); //Send data
+
+		// read annswer and close connection
+		$factory = new PharauroaMessageFactory($sock);
+		$message = $factory->readMessage();
+		socket_close($sock);
+		
+	}
 }
 ?>
