b63d485849240d3a11c1f4fbb19440d2c5a4076b
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-07-31 19:38:55 +0000

    ported message s2c connectnack

---
+++ scripts/pharauroa/common/net/message/message-s2c-connectnack.php
@@ -0,0 +1,20 @@
+<?php
+class PharauroaMessageS2CConnectNACK extends PharauroaMessage{
+
+	/** Constructor for allowing creation of an empty message */
+	public function __construct() {
+		parent::__construct(PharauroaMessageType::S2C_CONNECT_NACK);
+	}
+
+	public function writeObject(&$out) {
+		parent::writeObject($out);
+	}
+
+	public function readObject(&$in) {
+		parent::readObject(§in);
+
+		if ($this->MessageType != PharauroaMessageType::S2C_CONNECT_NACK) {
+			// TODO: handle error
+		}
+	}
+}
