180cd86e4a48c3b6247d850aeebfcd8330eb73b1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-08-15 16:18:18 +0000

    ask to log in

--- content/account/myaccount.php
+++ content/account/myaccount.php
@@ -8,14 +8,20 @@ class MyAccountPage extends Page {
 	}
 
 	function writeContent() {
+		if (!isset($_SESSION['username'])) {
+			startBox("Login Required");
+			echo '<p>Please <a href="'.STENDHAL_LOGIN_TARGET.'/index.php?id=content/account/login">login</a>.</p>';
+			endBox();
+			return;
+		}
 
 startBox("My Account"); ?>
 	<p>You are logged in as <b><?php echo htmlspecialchars($_SESSION['username']);?></b>.</p>
 <ul id="dmenu" >
 	<?php 
-	    echo '<li><a href="'.rewriteURL('/account/mycharacters.html').'"><img src="/images/buttons/players_button.png" alt=" "> My Characters</a></li>';
-	    echo '<li><a href="'.rewriteURL('/account/messages.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Messages</a></li>';
-	    echo '<li><a href="'.rewriteURL('/account/history.html').'"><img src="/images/buttons/history_button.png" alt=" "> Login History</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/mycharacters.html').'"><img src="/images/buttons/players_button.png" alt=" "> My Characters</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/messages.html').'"><img src="/images/buttons/postman_button.png" alt=" "> Messages</a></li>';
+		echo '<li><a href="'.rewriteURL('/account/history.html').'"><img src="/images/buttons/history_button.png" alt=" "> Login History</a></li>';
 		echo '<li><a href="'.rewriteURL('/account/change-password.html').'"><img src="/images/buttons/password_button.png" alt=" "> New Password</a></li>';
 		echo '<li><a href="'.rewriteURL('/account/merge.html').'"><img src="/images/buttons/merge_button.png" alt=" "> Merge Accounts</a> - (<a href="https://stendhalgame.org/wiki/Stendhal_Account_Merging">Help</a>)</li>';
 		echo '<li><a href="'.rewriteURL('/account/logout.html').'"><img src="/images/buttons/logout_button.png" alt=" "> Logout</a></li>';
