2927e9f0e2b826a5b2a5159bc8aeb0067c6e0693
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-02 19:07:29 +0000

    dump database result

--- content/admin/bingo.php
+++ content/admin/bingo.php
@@ -10,6 +10,7 @@ class BingoPage extends Page {
 
 		
         $row=mysql_fetch_assoc($result);
+        var_dump($row);
 		$this->id = $row['id'];
 		$this->name = $row['param1'];
 
@@ -26,7 +27,6 @@ class BingoPage extends Page {
 		startBox('Bingo');
 
 		$monsters = getMonsters();
-		echo 'Name: ' . htmlspecialchars($this->name);
 		foreach($monsters as $m) {
 			if($m->name==$this->name) {
 			?>
