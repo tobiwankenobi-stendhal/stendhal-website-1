40ed87d748cf67eb723b1b65a8c9f5b58b564c0f
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-02 19:24:17 +0000

    stripped page down to speed it up over slow wlan

--- content/admin/bingo.php
+++ content/admin/bingo.php
@@ -15,6 +15,19 @@ class BingoPage extends Page {
         mysql_free_result($result);
 	}
 	
+	public writeHttpHeader() {
+		?>
+		<!DOCTYPE title PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+		<html>
+			<head>
+			<meta http-equiv="Content-Type" content="text/html; charset=utf-8">		
+		<?php
+			$this->writeHtmlHeader();
+			$this->writeContent();
+		
+		return false;
+	}
+	
 	public function writeHtmlHeader() {
 		echo '<title>Bingo'.STENDHAL_TITLE.'</title>';
 		echo '<meta http-equiv="refresh" content="10; URL=/index.php?id=content/admin/bingo&amp;lastid='.htmlspecialchars($this->id).'">';
