9f3a0e29819b3f187c4ec0b1f3d75b875a82fe74
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-09-29 21:25:04 +0000

    moved localhost as special case to second place

--- tests/lib/openid/lightopenidTest.php
+++ tests/lib/openid/lightopenidTest.php
@@ -5,8 +5,8 @@ class LightOpenidTest extends PHPUnit_Framework_TestCase {
 
 	public function testhostExists() {
 		$openid = new LightOpenID();
-		$this->assertTrue($openid->hostExists("localhost"));
 		$this->assertTrue($openid->hostExists("www.google.com"));
+		$this->assertTrue($openid->hostExists("localhost"));
 		$this->assertTrue($openid->hostExists("ip6-loopback"));
 		$this->assertTrue($openid->hostExists("127.0.0.1"));
 		$this->assertFalse($openid->hostExists("not.exists"));
