496c3945f2d4201eed2297d45b0e533d5aa11fbe
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-10-22 16:08:46 +0000

    fixed regex delimeters

--- content/game/chat.php
+++ content/game/chat.php
@@ -56,7 +56,7 @@ for ($i = 0; $i < count($lines); $i++) {
 	} 
 
 	$line = htmlspecialchars($line);
-	$line = preg_replace('@', '&lt;(a)&gt;', $line);
+	$line = preg_replace('/@/', '&lt;(a)&gt;', $line);
 	$line = preg_replace('!(http|https)://(stendhalgame.org|arianne.sf.net|arianne.sourceforge.net|sourceforge.net|sf.net)(/[^ ]*)?!', '<a href="$1://$2$3">$1://$2$3</a>', $line);
 
 	if ($line != '') {
