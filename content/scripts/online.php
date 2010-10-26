8504822753a79771a4a7536d7b08d6e42ab076ab
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-10-26 21:33:41 +0000

    reduced space

--- content/scripts/online.php
+++ content/scripts/online.php
@@ -14,7 +14,7 @@ class OnlinePage extends Page {
 		if(sizeof($players)==0) {
 			echo 'There are no players logged in at the moment.';
 		}
-		echo '<div style="height: '.((floor(count($players) / 7) + 1) * 100) .'px">';
+		echo '<div style="height: '.((floor(count($players) / 7) + 1) * 90) .'px">';
 		foreach($players as $p) {
 			echo '<div class="onlinePlayer onlinePlayerHeight">';
 			echo '  <a class = "onlineLink" href="'.rewriteURL('/character/'.surlencode($p->name).'.html').'">';
