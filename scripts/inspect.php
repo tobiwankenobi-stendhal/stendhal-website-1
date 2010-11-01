2fc1f5210211cdf94b586facaa6a68c6cf4cf1ce
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-11-01 19:29:34 +0000

    added support for multiple character results in one script execution

--- scripts/inspect.php
+++ scripts/inspect.php
@@ -23,22 +23,25 @@ class InspectParser {
 	public function parse() {
 		$this->res = array();
 		$this->inspectStartIndex = -1;
+		$first = true;
 		for ($i = 0; $i < count($this->data); $i++) {
 			$line = $this->data[$i];
+
+			if (preg_match('/\[..:..\] Script "DeepInspect.class" was successfully executed./', $line)
+				|| (preg_match('/\[..:..\] Inspecting .*/', $line) && !$first)) {
+
+				$this->current = array();
+				$this->inspectEndIndex = $i -1;
+
+				$this->parseDeepInspect();
+				$this->res[] = $this->current;
+				$this->inspectStartIndex = -1;
+			}
 
 			if ($this->inspectStartIndex < 0) {
 				if (preg_match('/\[..:..\] Inspecting .*/', $line)) {
 					$this->inspectStartIndex = $i;
-				}
-			} else {
-				if (preg_match('/\[..:..\] Script "DeepInspect.class" was successfully executed./', $line)) {
-					$this->current = array();
-					$this->inspectEndIndex = $i -1;
-
-					$this->parseDeepInspect();
-
-					$this->res[] = $this->current;
-					$this->inspectStartIndex = -1;
+					$first = false;
 				}
 			}
 		}
