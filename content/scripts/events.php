15aa92b108794492fe8d0ab58c4a0f4ce1ba1944
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-11-04 20:24:15 +0000

    added ?test=beta parameter to include achievements (fixes #3102207)

--- content/scripts/events.php
+++ content/scripts/events.php
@@ -63,7 +63,7 @@ class EventsPage extends Page {
 		global $cache;
 		if (!isset($_REQUEST['profiling'])) {
 			$events = $cache->fetchAsArray('stendhal_events_'.$this->filter.'_'.$_SESSION['account']->id);
-			if (!isset($events)) {
+			if (!isset($events) || $_REQUEST['test']) {
 				$events=array_merge(getKillEvents($this->filter),
 					getQuestEvents($this->filter),
 					getLevelEvents($this->filter),
@@ -71,10 +71,12 @@ class EventsPage extends Page {
 					getPoisonEvents($this->filter),
 					getChangeZoneEvents($this->filter),
 					getOutfitEvents($this->filter),
-					getEquipEvents($this->filter),
-					getAchievementEvents($this->filter));
-					
-				$cache->store('stendhal_events_'.$this->filter.'_'.$_SESSION['account']->id, new ArrayObject($events), 60);
+					getEquipEvents($this->filter));
+				if ($_REQUEST['test']) {
+					$events=array_merge($events, getAchievementEvents($this->filter));
+				} else {
+					$cache->store('stendhal_events_'.$this->filter.'_'.$_SESSION['account']->id, new ArrayObject($events), 60);
+				}
 			}
 		} else {
 			echo '<pre>';
