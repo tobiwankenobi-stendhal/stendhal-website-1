752efcb796da851c6a277471f504a1975fb351cc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-11-07 00:32:15 +0000

    use http location header instead of broke meta-refresh redirect (fixes #3104408)

--- content/account/login.php
+++ content/account/login.php
@@ -3,73 +3,59 @@
 require_once('scripts/account.php');
 
 class LoginPage extends Page {
+	private $error;
 	
 	public function writeHttpHeader() {
-		return $this->handleRedirectIfAlreadyLoggedIn();
-	}
-
-	public function writeHtmlHeader() {
-		echo '<title>Login'.STENDHAL_TITLE.'</title>';
-		echo '<meta name="robots" content="noindex">'."\n";
-	}
-
-	function writeContent() {
-		
-		/**
-		 * Checks to see if the user has submitted his
-		 * username and password through the login form,
-		 * if so, checks authenticity in database and
-		 * creates session.
-		 */
-		$showLoginForm = true;
-		if(isset($_POST['sublogin'])) {
-			$showLoginForm = ! $this->checkLoginForm();
-		}
-		if ($showLoginForm) {
-			$this->displayLoginForm();
-		}
-	}
-
-	function handleRedirectIfAlreadyLoggedIn() {
-		if (isset($_REQUEST['url']) && checkLogin()) {
-			header('Location: '.STENDHAL_LOGIN_TARGET.$this->getUrl());
-			return false;
-		}
-		return true;
-	}
-
-
-	function checkLoginForm() {
-			/* Check that all fields were typed in */
-		if(!$_POST['user'] || !$_POST['pass']) {
-			startBox("Login failed");
-			echo "<span class=\"error\">You didn't fill in a required field.</span>";
-			endBox();
+		if ($this->handleRedirectIfAlreadyLoggedIn()) {
 			return false;
 		}
 
-		/* Spruce up username, check length */
+		return !$this->verifyLoginByPassword();
+	}
+
+	public function verifyLoginByPassword() {
+		if (!isset($_POST['sublogin'])) {
+			return false;
+		}
+
+		if( !$_POST['user'] || !$_POST['pass']) {
+			$this->error = "You didn't fill in a required field.";
+			return false;
+		}
+
 		$username = trim($_POST['user']);
 		$password = trim($_POST['pass']);
-
 		$result = Account::tryLogin("password", $username, $password);
 
 		if (! ($result instanceof Account)) {
-			startBox("Login failed");
-			echo '<span class="error">'.htmlspecialchars($result).'</span>';
-			endBox();
+			$this->error = $result;
 			return false;
 		}
 
 		/* Username and password correct, register session variables */
 		$_SESSION['account'] = $result;
 		$_SESSION['csrf'] = createRandomString();
-
-		echo "<meta http-equiv=\"Refresh\" content=\"1;url=".htmlspecialchars($this->getUrl())."\">";
-		startBox("Login");
-		echo '<h1>Login correct.</h1> Please wait...';
-		endBox();
+		header('Location: '.STENDHAL_LOGIN_TARGET.$this->getUrl());
 		return true;
+	}
+
+	public function writeHtmlHeader() {
+		echo '<title>Login'.STENDHAL_TITLE.'</title>';
+		echo '<meta name="robots" content="noindex">'."\n";
+		echo '<script src="'.STENDHAL_FOLDER.'/css/jquery-00000001.js" type="text/javascript"></script>';
+		echo '<script src="'.STENDHAL_FOLDER.'/css/openid-00000002.js" type="text/javascript"></script>';
+	}
+
+	function writeContent() {
+		$this->displayLoginForm();
+	}
+
+	function handleRedirectIfAlreadyLoggedIn() {
+		if (isset($_REQUEST['url']) && checkLogin()) {
+			header('Location: '.STENDHAL_LOGIN_TARGET.$this->getUrl());
+			return true;
+		}
+		return false;
 	}
 
 	function getUrl() {
@@ -95,12 +81,16 @@ class LoginPage extends Page {
 			Remember not to disclose your username or password to anyone, not even friends or administrators.<br>
 			Check that this webpage URL matches your game server name.
 		</div>
+
+		<?php
+		if ($this->error) {
+			echo "<p class=\"error\">".htmlspecialchars($this->error)."</p>";
+		}?>
+
 		<form action="" method="post">
 			<table>
 				<tr><td><label for="user">Username:</label></td><td><input type="text" id="user" name="user" maxlength="30"></td></tr>
 				<tr><td><label for="pass">Password:</label></td><td><input type="password" id="pass" name="pass" maxlength="30"></td></tr>
-				<!-- <tr><td colspan="2" align="left"><input type="checkbox" id="remember" name="remember">
-				<label for="remember"><font size="2">Remember me next time</font></label></td></tr> -->
 				<tr><td colspan="2" align="right"><input type="submit" name="sublogin" value="Login"></td></tr>
 			</table>
 
@@ -115,6 +105,58 @@ class LoginPage extends Page {
 		<?php
 		endBox();
 
+		if ($_REQUEST['test']) {
+			startBox("Open ID");
+			?>
+				<form id="openid_form" action="<?php echo STENDHAL_FOLDER.'/index.php?id=content/account/openid'?>" method="post">
+		<input id="oauth_version" name="oauth_version" type="hidden">
+		<input id="oauth_server" name="oauth_server" type="hidden">
+		<?php
+		if ($_REQUEST['merge']) {
+			echo '<input id="merge" name="merge" type="hidden" value="true">';
+		}
+		?>
+
+		<div id="openid_choice">
+			<p>Do you already have an account on one of these sites?</p>
+			<div id="openid_btns"></div>
+		</div>
+
+		<div id="openid_input_area"></div>
+		<div>
+			<noscript>
+				<p>OpenID is a service that allows you to log on to many different websites using a single identity.</p>
+			</noscript>
+		</div>
+
+		<p>Or, you can manually enter your OpenID</p>
+		<table id="openid-url-input">
+		<tbody><tr>
+			<td class="vt large">
+				<input id="openid_identifier" name="openid_identifier" class="openid-identifier" style="height: 28px; width: 450px;" tabindex="100" type="text">
+			</td>
+
+			<td class="vt large">
+				<input id="submit-button" style="margin-left: 5px; height: 36px;" value="Log in" tabindex="101" type="submit">
+			</td>
+		</tr></tbody>
+		</table>
+	</form>
+
+	<script type="text/javascript">
+		$().ready(function() {
+			openid.init('openid_identifier');
+		});
+	</script>
+
+<?php
+
+	if (isset($this->error)) {
+		echo '<div class="error">'.htmlspecialchars($this->error).'</div>';
+	}
+
+		endBox();
+		}
 	}
 }
 $page = new LoginPage();
