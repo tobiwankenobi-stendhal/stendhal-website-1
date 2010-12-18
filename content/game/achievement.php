e8c9ce1342110eba5c904f4a2064587d1715b7c0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2010-12-18 19:29:01 +0000

    images for achievements

--- content/game/achievement.php
+++ content/game/achievement.php
@@ -11,7 +11,7 @@ class AchievementPage extends Page {
 		echo '<table class="prettytable">';
 		foreach ($list as $achievement) {
 			echo '<tr>';
-			echo '<td>'.htmlspecialchars($achievement->category).'</td>';
+			echo '<td><img src="/images/achievements/'.htmlspecialchars($achievement->category).'.png" title="'.htmlspecialchars($achievement->category).'"></td>';
 			echo '<td><abbr title="'.htmlspecialchars($achievement->description).'">'.htmlspecialchars($achievement->title).'</abbr></td>';
 			echo '<td>'.htmlspecialchars($achievement->count).'</td>';
 			echo '</tr>';
