49d4ac1af139a6e4668feba199ead7ef0fb930c3
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-03-01 20:11:12 +0000

    started to work on achievement detail page

--- content/game/achievement.php
+++ content/game/achievement.php
@@ -1,15 +1,57 @@
 <?php 
 class AchievementPage extends Page {
+	private $achievements;
+
+	public function __construct() {
+		if ($_REQUEST['name']) {
+			$this->achievements = Achievement::getAchievement($_REQUEST['name']);
+		} else {
+			$this->achievements = Achievement::getAchievements();
+		}
+	}
+
+	public function writeHttpHeader() {
+		if ($_REQUEST['name'] && count($this->achievements)==0) {
+			header('HTTP/1.0 404 Not Found');
+			return true;
+		}
+		return true;
+	}
 
 	public function writeHtmlHeader() {
 		echo '<title>Achievements'.STENDHAL_TITLE.'</title>';
 	}
 
 	function writeContent() {
+		if ($_REQUEST['name']) {
+			if (count($this->achievements)==0) {
+				startBox('Achievement');
+				echo 'Achievement not found.';
+				endBox();
+			} else {
+				$this->achievementDetail();
+			}
+		} else {
+			$this->achievementList();
+		}
+	}
+
+	function achievementDetail() {
 		startBox("Achievement");
-		$list = Achievement::getAchievements();
 		echo '<table class="prettytable">';
-		foreach ($list as $achievement) {
+			echo '<tr>';
+			echo '<td><img src="/images/achievements/'.htmlspecialchars(strtolower($this->achievements->category)).'.png" title="'.htmlspecialchars($this->achievements->category).'"></td>';
+			echo '<td><abbr title="'.htmlspecialchars($this->achievements->description).'">'.htmlspecialchars($this->achievements->title).'</abbr></td>';
+			echo '<td>'.htmlspecialchars($this->achievements->count).'</td>';
+			echo '</tr>';
+		echo '</table>';
+		endBox();
+	}
+
+	function achievementList() {
+		startBox("Achievements");
+		echo '<table class="prettytable">';
+		foreach ($this->achievements as $achievement) {
 			echo '<tr>';
 			echo '<td><img src="/images/achievements/'.htmlspecialchars(strtolower($achievement->category)).'.png" title="'.htmlspecialchars($achievement->category).'"></td>';
 			echo '<td><abbr title="'.htmlspecialchars($achievement->description).'">'.htmlspecialchars($achievement->title).'</abbr></td>';
