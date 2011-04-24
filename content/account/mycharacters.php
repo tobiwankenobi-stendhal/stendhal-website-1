5dca6cd77860f69874b1fc6a030da9a93e1800da
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-04-24 22:11:28 +0000

    make alpha game window large enough

--- content/account/mycharacters.php
+++ content/account/mycharacters.php
@@ -63,7 +63,7 @@ class MyCharactersPage extends Page {
 				echo '  <span class="block">Age: '.intval($p->age/60).' h</span>';
 				echo '  <span class="block"><a class = "characterLink" href="'.rewriteURL('/character/'.surlencode($p->name).'.html').'">Details</a></span>';
 				if ($adminLevel > 0) {
-					echo '  <span class="block"><a class = "characterLink" onclick=\'window.open("/client/stendhal.html#'.surlencode($p->name).'", "stendhalclient", "directories=0,height=600,location=0,menubar=0,scrollbars=n,status=n,toolbar=n,width=1024", false);return false;\' href="/client/stendhal.html#'.surlencode($p->name).'">Alpha</a></span>';
+					echo '  <span class="block"><a class = "characterLink" onclick=\'window.open("/client/stendhal.html#'.surlencode($p->name).'", "_blank", "directories=0,height=750,location=0,left=1,top=1,menubar=0,scrollbars=n,status=n,toolbar=n,width=1000", false);return false;\' href="/client/stendhal.html#'.surlencode($p->name).'">Alpha</a></span>';
 				}
 				echo '</div>';
 			}
