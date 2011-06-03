abd7b258379f3dabdcbbd58f206b87c42c92f682
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-06-03 10:40:47 +0000

    a simple openid consumer that is hard wired to the stendhalserver and gets details by directly looking at the stendhal database

---
+++ content/association/login.php
@@ -0,0 +1,110 @@
+<?php
+/*
+ Stendhal website - a website to manage and ease playing of Stendhal game
+ Copyright (C) 2008-2010 The Arianne Project
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once('scripts/account.php');
+require_once('content/account/openid.php');
+
+class LoginPage extends Page {
+	private $error;
+	private $openid;
+
+	public function writeHttpHeader() {
+		if ($this->handleRedirectIfAlreadyLoggedIn()) {
+			return false;
+		}
+
+		// force SSL if supported
+		if (strpos(STENDHAL_LOGIN_TARGET, 'https://') !== false) {
+			if (!isset($_SERVER['HTTPS']) || ($_SERVER['HTTPS'] != "on")) {
+				header('Location: '.STENDHAL_LOGIN_TARGET.rewriteURL('/association/login.html'));
+				return false;
+			}
+		}
+
+		$this->openid = new OpenID();
+		if (!isset($_GET['openid_mode'])) {
+			// redirect to openid provider?
+			$this->openid->doOpenidRedirectIfRequired(/*'https://stendhalgame.org'*/'http://localhost');
+			if ($this->openid->isAuth && !$this->openid->error) {
+				return false;
+			}
+
+		} else {
+
+			if ($this->verifyLoginByOpenid()) {
+				return false;
+			}
+
+		}
+
+		return true;
+	}
+
+	public function verifyLoginByOpenid() {
+		if (!isset($_GET['openid_mode'])) {
+			return false;
+		}
+
+		if($_GET['openid_mode'] == 'cancel') {
+			$this->openid->error = 'OpenID-Authentication was canceled.';
+			return false;
+		}
+
+		var_dump($this->openid->getStendhalAccountName());
+		
+		die('finished');
+		
+		/*
+		$accountLink = $this->openid->createAccountLink();
+		if (!$accountLink) {
+			$this->openid->error = 'OpenID-Authentication failed.';
+			return false;
+		}
+		$this->openid->succesfulOpenidAuthWhileNotLoggedIn($accountLink);
+		header('Location: '.STENDHAL_LOGIN_TARGET.$this->getUrl());
+		return true;*/
+	}
+
+	function handleRedirectIfAlreadyLoggedIn() {
+		if (checkLogin()) {
+			if (isset($_REQUEST['url'])) {
+				header('Location: '.STENDHAL_LOGIN_TARGET.$this->getUrl());
+			} else {
+				header('Location: '.STENDHAL_LOGIN_TARGET);
+			}
+			return true;
+		}
+		return false;
+	}
+
+	function getUrl() {
+		$url = $_REQUEST['url'];
+		if (strpos($url, '/') !== 0) {
+			$url = '/'.$url;
+		}
+		// prevent header splitting
+		if (strpos($url, '\r') || strpos($url, '\n')) {
+			$url = '/';
+		}
+		return $url;
+	}
+
+}
+$page = new LoginPage();
+?>
