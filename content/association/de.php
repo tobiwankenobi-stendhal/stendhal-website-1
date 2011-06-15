89a614dc51694e69594314c30802e303e54e4188
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-06-15 21:33:59 +0000

    some improvements to the German translation, mostly suggested by bluelads99

--- content/association/de.php
+++ content/association/de.php
@@ -13,7 +13,7 @@ function loadLanguage() {
 	$dict['Concept'] = 'Konzept';
 	$dict['Projects'] = 'Projekte';
 	$dict['Modules/Material'] = 'Module/Material';
-	$dict['Chat'] = 'Plausch';
+	$dict['Chat'] = 'Besprechen';
 	$dict['Events'] = 'Ereignisse';
 	$dict['Language'] = 'Sprache';
 	$dict['Share'] = 'Empfehlen';
@@ -23,14 +23,17 @@ function loadLanguage() {
 	$dict['Editing'] = 'Bearbeiten';
 	$dict['You need to'] = 'Sie müssen sich';
 	$dict['login'] = 'anmelden';
-	$dict['in order to edit pages.'] = 'um Seiten zu redigieren';
+	$dict['in order to edit pages.'] = 'um Seiten zu bearbeiten.';
 	$dict['Editing'] = 'Bearbeiten';
-	$dict['Please skip Heading 1 and start with Heading 2.'] = 'Bitte übergehe Überschrift 1 und Starte mit Überschrift 2';
+	$dict['Please skip Heading 1 and start with Heading 2.'] = 'Bitte überspringe Überschrift 1 und beginne mit Überschrift 2';
 	$dict['Back to article'] = 'Zurück zum Beitrag';
 	$dict['Error'] = 'Fehler';
 	$dict['Session information was lost. Please save again.'] = 'Die Sitzungsinformationen gingen verloren. Bitte nochmals speichern';
 	$dict['Documents'] = 'Dokumente';
 	$dict['Sorry, internal documents are only available to members.'] = 'Leider sind interne Dokumente nur für Mitglieder verfügbar.';
 	$dict['Sorry, the requested document was not found.'] = 'Leider wurde das angeforderte Dokument nicht gefunden.';
+	$dict['Sorry, the requested page does not exist.'] = 'Leider existiert diese Seite nicht.';
 	$dict['These are the internal documents of the association.'] = 'Dies sind die internen Unterlagen des Vereins.';
+	$dict['Last edited on'] = 'Zuletzt geändert am';
+	$dict['by account'] = 'durch Benutzer';
 }
