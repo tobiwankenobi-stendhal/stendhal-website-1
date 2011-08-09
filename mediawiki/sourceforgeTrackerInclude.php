8c83d6f1ba08e8a730c4f2a147c4a4cdbe7982f1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-08-09 22:08:11 +0000

    converted into a MediaWiki extension

--- mediawiki/sourceforgeTrackerInclude.php
+++ mediawiki/sourceforgeTrackerInclude.php
@@ -16,26 +16,46 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-$url = 'http://sourceforge.net/search/index.php?group_id=1111&type_of_search=artifact&q=&artifact_id=3389093+3388444+3388024&submitted_by=&assigned_to=&open_date_start=&open_date_end=&last_update_date_start=&last_update_date_end=&form_submit=Search&limit=100';
+$wgExtensionFunctions[] = "sourceforgeTrackerIncludeSetup";
+$wgExtensionCredits['other'][] = array(
+	'version' => '0.1',
+	'name' => 'Sourceforge Tracker Include',
+	'author' => 'Hendrik Brummermann',
+	'url' => 'http://arianne.sf.net',
+	'description' => 'Includes results from a sourceforge tracker search'
+);
 
-// get search result
-$data = file_get_contents($url);
+/**
+* setup the parser by telling it about the tags we can
+* handle and include the required java script
+*/
+function sourceforgeTrackerIncludeSetup() {
+	global $wgParser, $wgScriptPath, $wgOut;
+	$wgParser->setHook('tracker', 'sourceforgeTrackerInclude');
+}
 
-// strip head and foot
-$needle = '<!-- 0 -->'; //<caption>Search Results</caption>';
-$pos = strpos($data, $needle) + strlen($needle);
-$data = substr($data, $pos, -1);
-$needle = '</table>';
-$pos = strpos($data, $needle) + strlen($needle);
-$data = substr($data, 0, $pos);
+function sourceforgeTrackerInclude($input, $argv, $parser) {
+	$parser->disableCache();
 
-// fix links
-$data = preg_replace('|<a href="|', '<a href="https://sourceforge.net', $data);
+	$url = 'http://sourceforge.net/search/index.php?group_id=1111&type_of_search=artifact&q=&artifact_id=3389093+3388444+3388024&limit=100';
 
-// add heading
-$prefix = '<table><tr><th>Priority</th><th>ID</th><th>Tracker</th><th>Summary</th><th>Assignee</th><th>Submitter</th><th>Status</th><th>Opened</th></tr>';
-$data = $prefix.$data;
+	// get search result
+	$data = file_get_contents($url);
 
-echo $data;
+	// strip head and foot
+	$needle = '<!-- 0 -->'; //<caption>Search Results</caption>';
+	$pos = strpos($data, $needle) + strlen($needle);
+	$data = substr($data, $pos, -1);
+	$needle = '</table>';
+	$pos = strpos($data, $needle) + strlen($needle);
+	$data = substr($data, 0, $pos);
 
+	// fix links
+	$data = preg_replace('|<a href="|', '<a href="https://sourceforge.net', $data);
 
+	// add heading
+	$prefix = '<table class="prettytable sortable"><tr><th>Prio</th><th>ID</th><th>Tracker</th><th>Summary</th><th>Assignee</th><th>Submitter</th><th>Status</th><th>Opened</th></tr>';
+	$data = $prefix.$data;
+
+	return $data;
+}
