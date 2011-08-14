5a8f64122d5bc9bfb0e91a19fdbe1634c1293212
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-08-14 20:30:36 +0000

    fixed php syntax error

--- content/game/achievement.php
+++ content/game/achievement.php
@@ -13,7 +13,7 @@ class AchievementPage extends Page {
 	}
 
 	public function writeHttpHeader() {
-		if ($isset($_REQUEST['name']) && $_REQUEST['name'] != 'special' && count($this->achievements)==0) {
+		if (isset($_REQUEST['name']) && $_REQUEST['name'] != 'special' && count($this->achievements)==0) {
 			header('HTTP/1.0 404 Not Found');
 			return true;
 		}
