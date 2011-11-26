afa8c23dd124e311dae505311b003c6d4f1cc5ad
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-11-26 08:39:37 +0000

    add a second box with trace details instead of replacing the first one

--- content/scripts/netstats.php
+++ content/scripts/netstats.php
@@ -8,20 +8,27 @@ class NetstatsPage extends Page {
 
 	function writeContent() {
 		startBox('Trace route');
-		echo '<div id="traceresult">Tracing route, please wait...</div>';
+		echo '<div id="traceresult1">Tracing route, please wait...</div>';
 		endBox();
+
+		echo '<div id="tracebox2" style="display:none">';
+		startBox('Details');
+		echo '<div id="traceresult2">Tracing route, please wait...</div>';
+		endBox();
+		echo' </div>';
 		?>
 <script type="text/javascript">
 $().ready(function() {
 	$.ajax({url: "/index.php?id=content/scripts/api&method=traceroute&fast=1",
 		dataType: 'html',
 		success: function(data) {
-		$('#traceresult').html(data);
+		$('#traceresult1').html(data);
+		$('#tracebox2').css('display', 'block');
 
 		$.ajax({url: "/index.php?id=content/scripts/api&method=traceroute&fast=0",
 			dataType: 'html',
 			success: function(data) {
-			$('#traceresult').html(data);
+			$('#traceresult2').html(data);
 		}});
 	}});
 });
