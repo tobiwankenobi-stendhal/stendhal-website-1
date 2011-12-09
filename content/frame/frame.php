b4a684f79145c5d420555b2abfd8ee6ade0d5677
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-12-09 22:39:55 +0000

    updated jquery to latest version 1.7.1 because the old 1.4 version was causing bogus content security policy warnings

--- content/frame/frame.php
+++ content/frame/frame.php
@@ -54,7 +54,7 @@ abstract class PageFrame {
 	 * includes java script libraries
 	 */
 	public function includeJs() {
-		echo '<script type="text/javascript" src="'.STENDHAL_FOLDER.'/css/jquery-00000001.js"></script>';
+		echo '<script type="text/javascript" src="'.STENDHAL_FOLDER.'/css/jquery-00000002.js"></script>';
 		echo '<script type="text/javascript" src="'.STENDHAL_FOLDER.'/css/jquery.tooltip-merged.min.js"></script>';
 		echo '<script type="text/javascript" src="'.STENDHAL_FOLDER.'/css/00000006.js"></script>';
 	}
