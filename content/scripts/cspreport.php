3aae2692427d2da1888d58d7c1f35c117b1d9dbd
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-12-10 11:18:36 +0000

    removed debug code

--- content/scripts/cspreport.php
+++ content/scripts/cspreport.php
@@ -3,8 +3,7 @@
 class CSPReportPage extends Page {
 
 	public function writeHttpHeader() {
-		var_dump($_SERVER);
-//		header('HTTP/1.1 204');
+		header('HTTP/1.1 204');
 		$sql = "INSERT INTO content_security_policy_report (address, useragent, content) "
 			." VALUES ('".mysql_real_escape_string($_SERVER['REMOTE_ADDR'])."', '"
 			. mysql_real_escape_string($_SERVER['HTTP_USER_AGENT'])."', '"
