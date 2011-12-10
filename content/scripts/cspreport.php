f25e20680aeb07b673c246102a5b77d1602a5cb0
Hendrik Brummermann <nhnb@users.sourceforge.net>
2011-12-10 11:17:04 +0000

    fixed user agent logging of CSP violation reports

--- content/scripts/cspreport.php
+++ content/scripts/cspreport.php
@@ -3,10 +3,11 @@
 class CSPReportPage extends Page {
 
 	public function writeHttpHeader() {
-		header('HTTP/1.1 204');
+		var_dump($_SERVER);
+//		header('HTTP/1.1 204');
 		$sql = "INSERT INTO content_security_policy_report (address, useragent, content) "
 			." VALUES ('".mysql_real_escape_string($_SERVER['REMOTE_ADDR'])."', '"
-			. mysql_real_escape_string($_SERVER['USER_AGENT'])."', '"
+			. mysql_real_escape_string($_SERVER['HTTP_USER_AGENT'])."', '"
 			. mysql_real_escape_string(file_get_contents('php://input'))."')";
 		mysql_query($sql, getWebsiteDB());
 		return false;
