d6273a6c561c24ac00e130d97ed9791e7aeb2499
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-02-26 20:46:20 +0000

    code format (no other changes)

--- content/account/approve.php
+++ content/account/approve.php
@@ -1,20 +1,18 @@
 <?php
-require_once('scritps/configuration.php');
-include_once('scripts/mysql.php');
 
 function createRandomPassword() {
-    $chars = "abcdefghijkmnopqrstuvwxyz023456789";
-    $i = 0;
-    $pass = '' ;
-    $amount=strlen($chars);
+	$chars = "abcdefghijkmnopqrstuvwxyz023456789";
+	$i = 0;
+	$pass = '' ;
+	$amount=strlen($chars);
 
-    while ($i <= 7) {
-        $num = rand() % $amount;
-        $pass = $pass . $chars[$num];
-        $i++;
-    }
-    
-    return $pass;
+	while ($i <= 7) {
+		$num = rand() % $amount;
+		$pass = $pass . $chars[$num];
+		$i++;
+	}
+
+	return $pass;
 }
 
 class ApprovePage extends Page {
@@ -23,67 +21,58 @@ class ApprovePage extends Page {
 		echo '<title>Approve Password Reset'.STENDHAL_TITLE.'</title>';
 		echo '<meta name="robots" content="noindex">'."\n";
 	}
-	
+
 	function writeContent() {
 
-if(!isset($_GET["sign"])) {
-  die('You didn\'t fill in a required field.');
-  }
+		if(!isset($_GET["sign"])) {
+			die('You didn\'t fill in a required field.');
+		}
 
-$signature=$_GET["sign"];
-$signature=mysql_real_escape_string($signature);
+		$signature=$_GET["sign"];
+		$signature=mysql_real_escape_string($signature);
 
-/*
- * Get the user name from the username<->hash relation
- */
-$query='select username from remind_password where confirmhash="'.$signature.'"';
-$result = mysql_query($query, getWebsiteDB());
+		// Get the user name from the username<->hash relation
+		$query='select username from remind_password where confirmhash="'.$signature.'"';
+		$result = mysql_query($query, getWebsiteDB());
 
-if(mysql_numrows($result)!=1) {
-  mysql_free_result($result);
+		if(mysql_numrows($result)!=1) {
+			mysql_free_result($result);
 
-  startBox("No such username");
-    ?>
-    We are unable to find a valid username associated to that email account.<p>
-    Your password can not be reset.
-    <p>
-    Back to <a href="?">Main</a>
-  <?php
-  endBox();
-} else {
-  $row=mysql_fetch_assoc($result);
-  $username=$row["username"];
+			startBox("No such username");
+			?>
+			We are unable to find a valid username associated to that email account.
+			<p>Your password can not be reset.
+			<p>Back to <a href="?">Main</a>
+			<?php
+			endBox();
 
-  mysql_free_result($result);
-  
-  /* Remove the entry or anything 48 hours old.*/
-  $q = "delete from remind_password where username = '".mysql_real_escape_string($username)."' or  datediff(now(),requested)>2";
-  $result = mysql_query($q,getWebsiteDB());
+		} else {
 
-  /*
-   * Create a random password for it and set it.
-   */
-  $newpassword=createRandomPassword();
+			$row=mysql_fetch_assoc($result);
+			$username=$row["username"];
+			mysql_free_result($result);
 
-  $md5newpass = strtoupper(md5($newpassword));
-  $q = "update account set password='".mysql_real_escape_string($md5newpass)."' where username = '".mysql_real_escape_string($username)."'";
-  $result = mysql_query($q,getGameDB());
+			// Remove the entry or anything 48 hours old.
+			$q = "delete from remind_password where username = '".mysql_real_escape_string($username)."' or  datediff(now(),requested)>2";
+			$result = mysql_query($q,getWebsiteDB());
 
-  /*
-   * Show user the new password.
-   */
-  startBox("New password generated");
-    ?>
-    Per your request we have reset the password of your account "<b><?php echo $username; ?></b>".<br>
-    Your new password is "<b><?php echo $newpassword; ?></b>".
-    <p>
-    Store it on a secure place.
-    <?php
-  endBox();
-  }   
+			// Create a random password for it and set it.
+			$newpassword=createRandomPassword();
 
+			$md5newpass = strtoupper(md5($newpassword));
+			$q = "update account set password='".mysql_real_escape_string($md5newpass)."' where username = '".mysql_real_escape_string($username)."'";
+			$result = mysql_query($q,getGameDB());
+
+			// Show user the new password.
+			startBox("New password generated");
+			?>
+			Per your request we have reset the password of your account "<b><?php echo $username; ?></b>".<br>
+			Your new password is "<b><?php echo $newpassword; ?></b>".
+			<p>Store it on a secure place.
+			<?php
+			endBox();
+		}
 	}
 }
+
 $page = new ApprovePage();
-?>
-  
--- content/account/remind.php
+++ content/account/remind.php
@@ -1,6 +1,5 @@
 <?php
 
-require_once('scripts/account.php');
 
 die ('Not implemented, yet');
 
@@ -13,95 +12,98 @@ class RemindPage extends Page {
 
 	function writeContent() {
 
-/*
-   It is composed of two steps:
-   a) User click new password and we email a md5(rand()) to him IF the account is registered with stendhal.
-   b) The user confirms the link and we effectively change the password.
- */
+		/**
+		 * It is composed of two steps:
+		 * a) User click new password and we email a md5(rand()) to him IF the account is registered with stendhal.
+		 * b) The user confirms the link and we effectively change the password.
+		 */
+		if(isset($_POST["forgotpassword"])) {
+			if(!isset($_POST["email"]) or !isset($_POST["account"])) {
+				die('You didn\'t fill in a required field.');
+			}
 
-if(isset($_POST["forgotpassword"])) {
-  if(!isset($_POST["email"]) or !isset($_POST["account"])) {
-    die('You didn\'t fill in a required field.');
-  }
-  
-  $email=mysql_real_escape_string($_POST["email"]);
-  
-  if(existsUser($email)) {
-    $signature=strtoupper(md5(rand()));
-    
-    /* Good, store it... */
-    $username=getUser($email);
-    
-    $query='insert into remind_password values("'.$username.'","'.$signature.'",null)';
-    if(!mysql_query($query, getWebsiteDB())) {
-      echo '<span class="error">There has been a problem while sending your password.</span>';
-      die();
-    }
-        
-    /* Remove the entry or anything 48 hours old.*/
-    $q = "delete from remind_password where datediff(now(),requested)>2";
-    $result = mysql_query($q,getWebsiteDB());
-    
-    /* ...and email */
-    $server=$_SERVER["SERVER_NAME"];
-    $location=str_replace("/index.php","",$_SERVER["PHP_SELF"]);
-    
-    $clientip=$_SERVER['REMOTE_ADDR'];
-    
-    $body=file_get_contents("login/remindpassword.email");
-    
-    /* Fill variables */
-    $body=str_replace("[SERVER]",$server.$location,$body);
-    $body=str_replace("[SIGNATURE]",$signature,$body);
-    $body=str_replace("[CLIENTIP]",$clientip,$body);
-    
-    if($body==false) {
-      echo '<span class="error">There has been a problem while getting password email template.</span>';
-      die();
-    }
-  
-    $headers = 'From: noreply@stendhalgame.org';  
-    if(!mail($email,"Password reset request",$body,$headers)) {
-      echo '<span class="error">There has been a problem while sending your password email.</span>';
-      die();
-    }
-    
-    startBox("Password reset link emailed");
-    ?>
-      We have just sent you a link to reset your password.<br>
-      Check you inbox and follow the email instructions.
-      <p>
-      Back to <a href="?">Main</a>
-    <?php
-    endBox();
-  } else {
-    startBox("Unregistered account");
-    ?>
-    The email that you have written is not registered in this server.<p>
-    If you don't remind your email there is no way of reseting your password.
-    <p>
-    Back to <a href="?">Main</a>
+			$email=mysql_real_escape_string($_POST["email"]);
 
-    <?php
-    endBox();
-  	}
-} else {
-  startBox("Forgot your password?");
-  ?>
-  In case you have forgotten your new password or your account information we can send you it to your email account that you used to create your stendhal account.<p>
-  <form action="" method="post">
-  <table>
-    <tr><td>Email address:</td><td><input type="text" name="email" maxlength="90"></td></tr>
-    <tr><td>Account:</td><td><input type="text" name="account" maxlength="90"></td></tr>
-    <tr><td><input type="checkbox" name="knowaccount" checked>I do remember my account name.</td></tr>
-    <tr><td colspan="2" align="right"><input type="submit" name="forgotpassword" value="Get new password"></td></tr>
-  </table>
-  </form>
+			if(existsUser($email)) {
+				$signature=strtoupper(md5(rand()));
 
-  <?php
-  endBox();
-  }
+				// Good, store it...
+				$username=getUser($email);
+				$query='insert into remind_password values("'.$username.'","'.$signature.'",null)';
+				if(!mysql_query($query, getWebsiteDB())) {
+					echo '<span class="error">There has been a problem while sending your password.</span>';
+					die();
+				}
+
+				// Remove the entry or anything 48 hours old.
+				$q = "delete from remind_password where datediff(now(),requested)>2";
+				$result = mysql_query($q,getWebsiteDB());
+
+				// ...and email
+				$server=$_SERVER["SERVER_NAME"];
+				$location=str_replace("/index.php","",$_SERVER["PHP_SELF"]);
+
+				$clientip=$_SERVER['REMOTE_ADDR'];
+
+				$body=file_get_contents("login/remindpassword.email");
+
+				// Fill variables
+				$body=str_replace("[SERVER]",$server.$location,$body);
+				$body=str_replace("[SIGNATURE]",$signature,$body);
+				$body=str_replace("[CLIENTIP]",$clientip,$body);
+
+				if ($body == false) {
+					echo '<span class="error">There has been a problem while getting password email template.</span>';
+					die();
+				}
+
+				$headers = 'From: noreply@stendhalgame.org';
+				if(!mail($email,"Password reset request",$body,$headers)) {
+					echo '<span class="error">There has been a problem while sending your password email.</span>';
+					die();
+				}
+
+				startBox("Password reset link emailed");
+				?>
+				We have just sent you a link to reset your password.<br>
+				Check you inbox and follow the email instructions.
+				<p>
+				Back to <a href="?">Main</a>
+				<?php
+				endBox();
+
+			} else {
+
+				startBox("Unregistered account");
+				?>
+				The email that you have written is not registered in this server.<p>
+				If you don't remind your email there is no way of reseting your password.
+				<p>
+				Back to <a href="?">Main</a>
+
+				<?php
+				endBox();
+			}
+
+		} else {
+
+			startBox("Forgot your password?");
+			?>
+			In case you have forgotten your new password or your account information we can send you it to your email account that you used to create your stendhal account.<p>
+			<form action="" method="post">
+			<table>
+			<tr><td>Email address:</td><td><input type="text" name="email" maxlength="90"></td></tr>
+			<tr><td>Account:</td><td><input type="text" name="account" maxlength="90"></td></tr>
+			<tr><td><input type="checkbox" name="knowaccount" checked>I do remember my account name.</td></tr>
+			<tr><td colspan="2" align="right"><input type="submit" name="forgotpassword" value="Get new password"></td></tr>
+			</table>
+			</form>
+			
+			<?php
+			endBox();
+		}
 	}
 }
+
 $page = new RemindPage();
-?>
+
