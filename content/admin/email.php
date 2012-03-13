57bd51546bdcc0d829248a4b5c614d46bc76a389
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-03-13 22:41:47 +0000

    an admin page to send emails as gm-review

---
+++ content/admin/email.php
@@ -0,0 +1,45 @@
+<?php
+class AdminEMailPage extends Page {
+	function writeContent() {
+
+		if(getAdminLevel() < 400) {
+			die("Ooops!");
+		}
+
+		if (isset($_POST['body'])) {
+			$this->sendMail();
+		} else {
+			$this->renderForm();
+		}
+	}
+
+	function renderForm() {
+		startBox("Email");
+		?>
+<form method="POST">
+<table>
+<tr><td><label for"subject">Subject:</label></td><td><input name="subject"></td></tr>
+<tr><td><label for"to">To:</label></td><td><input name="to"></td></tr>
+<!-- <tr><td><label for"cc">Cc:</label></td><td><input name="cc"></td></tr>  -->
+</table>
+<textarea name="body" cols="70" rows="20"></textarea>
+<input type="submit">
+</form>
+		<?php
+		endBox();
+	}
+
+	function sendMail() {
+		startBox("Email");
+		$res = mail($_REQUEST['to'], $_REQUEST['subject'], $_REQUEST['body'], 
+			'Cc: '.STENDHAL_GM_EMAIL."\r\nFrom: ".STENDHAL_GM_EMAIL,
+			'-f '.STENDHAL_GM_ADDRESS);
+		if ($res) {
+			echo 'Mail sent.';
+		} else {
+			echo 'Failed.';
+		}
+		endBox();
+	}
+}
+$page = new AdminEMailPage();
