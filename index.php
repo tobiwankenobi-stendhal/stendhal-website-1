c4e6ed02cedb1f88fc6ee0406d378877ede8edba
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-06-06 16:06:40 +0000

    increased cache buster

--- index.php
+++ index.php
@@ -109,7 +109,7 @@ header('Content-Type: text/html; charset=utf-8')
 <!DOCTYPE html>
 <html>
 	<head>
-	<link rel="stylesheet" type="text/css" href="<?php echo STENDHAL_FOLDER; ?>/css/00000026.css">
+	<link rel="stylesheet" type="text/css" href="<?php echo STENDHAL_FOLDER; ?>/css/00000027.css">
 	<!--[if lt IE 8]><link rel="stylesheet" type="text/css" href="<?php echo STENDHAL_FOLDER;?>/css/ie000012.css"><![endif]-->
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<?php
