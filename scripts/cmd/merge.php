e22653fb4df2a2e803d88f37931f23859a282f2e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-06-24 13:57:33 +0000

    check account status

--- scripts/cmd/merge.php
+++ scripts/cmd/merge.php
@@ -5,7 +5,7 @@ if (isset($_SERVER['SERVER_NAME'])) {
 	exit;
 }
 
-if (count($_SERVER['argv']) != 3) {
+if (count($_SERVER['argv']) < 3 || count($_SERVER['argv']) > 4) {
 	echo "Call php merge.php <oldaccount> <newaccount>.\r\n";
 	echo "    <oldaccount> is the one which will be deactivated.\r\n";
 	echo "    <newaccount> is the one which will get all characters.\r\n";
@@ -16,12 +16,13 @@ set_include_path('../..');
 
 require_once('scripts/website.php');
 
-echo "\r\n\r\n\r\nIgnore the above errors\r\n\r\n\r\n\r\n\r\n\r\n";
-
 connect();
 
-$oldAccountId = getUserID($_SERVER['argv'][1]);
-$newAccountId = getUserID($_SERVER['argv'][2]);
+$oldUsername = $_SERVER['argv'][1];
+$newUsername = $_SERVER['argv'][2];
+
+$oldAccountId = getUserID($oldUsername);
+$newAccountId = getUserID($newUsername);
 
 echo 'Old id: '.$oldAccountId. '      New id: '.$newAccountId."\r\n";
 
@@ -35,4 +36,30 @@ if ($oldAccountId == $newAccountId) {
 	exit;
 }
 
+$oldAccount = Account::readAccountByName($oldUsername);
+$newAccount = Account::readAccountByName($newUsername);
+
+$okay = true;
+if ($oldAccount->getAccountStatusMessage() != null) {
+	echo 'Status of old ' . $oldUsername . ': ' . $oldAccount->getAccountStatusMessage()."\r\n";
+	$okay = false;
+}
+
+if ($newAccount->getAccountStatusMessage() != null) {
+	echo 'Status of new ' . $newUsername . ': ' . $newAccount->getAccountStatusMessage()."\r\n";
+	$okay = false;
+}
+
+if (!$okay) {
+	if ((count($_SERVER['argv']) < 4) || $_SERVER['argv'][3] != '-force') {
+		echo "\r\nERROR: Not merged\r\n\r\n";
+		echo 'Use php merge.php ' . $oldUsername . ' ' , $newUsername . ' -force'."\r\n";
+		die();
+	} else {
+		echo "\r\nForced to ignore warnings\r\n";
+	}
+}
+
 mergeAccount($_SERVER['argv'][1], $_SERVER['argv'][2]);
+
+echo "\r\nMerged\r\n";
