3f25ad033a0672c6634045d4ad15c3aa7f7a39a1
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-08-19 18:22:05 +0000

    command line tool to send registration mails

---
+++ scripts/cmd/mail.php
@@ -0,0 +1,39 @@
+<?php
+
+if (!isset($_SERVER['SERVER_NAME'])) {
+	
+	if (count($_SERVER['argv']) != 5) {
+		echo "Call php mail.php player_id username token email.\r\n";
+		exit;
+	}
+	
+	set_include_path('../..');
+	
+	require_once('scripts/website.php');
+	
+	connect();
+	
+	$playerId = $_SERVER['argv'][1];
+	$username = $_SERVER['argv'][2];
+	$token = $_SERVER['argv'][3];
+	$email = $_SERVER['argv'][4];
+
+	sendRegistrationMail($playerId, $username, $token, $email);
+}
+
+function sendRegistrationMail($playerId, $username, $token, $email) {
+	$subject = "Stendhal account ".$username." email verification";
+	$body = "Hello ".$username."!\r\n";
+	$body .= "\r\n";
+	$body .= "please confirm your email-address by clicking on this link:\r\n";
+	$body .= STENDHAL_LOGIN_TARGET."/account/verify?token=".$token."\r\n";
+	$body .= "\r\n";
+	$body .= "\r\n";
+	$body .= "If you did not create a Stendhal account, please ignore this email.\r\n";
+	$body .= "Your email address will be used for account recovery (if you forget \r\n";
+	$body .= "(your password or if there are indications for an account hack).\r\n";
+	$body .= "\r\n";
+	$body .= "Best regards from the Stendhal team\r\n";
+	$body .= "\r\n";
+	mail($email, $subject, $body, "From: ".STENDHAL_NOREPLY_EMAIL, '-f '.STENDHAL_NOREPLY_EMAIL);
+}
