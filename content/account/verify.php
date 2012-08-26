8e4bfbac255e6a7deb93e9f2d7fd3293df9e0921
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-08-26 09:36:53 +0000

    layout of error messages

--- content/account/verify.php
+++ content/account/verify.php
@@ -6,20 +6,20 @@
 class VerifyPage extends Page {
 
 	public function writeHtmlHeader() {
-		echo '<title>E-Mail verification'.STENDHAL_TITLE.'</title>';
+		echo '<title>Email verification'.STENDHAL_TITLE.'</title>';
 		echo '<meta name="robots" content="noindex">'."\n";
 	}
 
 	function writeContent() {
-		startBox('E-Mail verification');
+		startBox('Verification of email-address');
 		if (!isset($_REQUEST['token'])) {
-			echo '<p>Error: The token is missing.</p>';
+			echo '<p class="error">Error: The token is missing.</p>';
 		} else {
 			$res = Account::verifyEMail($_REQUEST['token']);
 			if ($res) {
 				echo '<p>Your email address was confirmed successfully.</p>';
 			} else {
-				echo '<p>Error: The token is invalid.</p>';
+				echo '<p class="error">Error: The token is invalid.</p>';
 			}
 		}
 		endBox();
