b5d761ff1522bcd5a16c8449408621312af438c6
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-09-02 20:35:44 +0000

    wrote mail texts for account merge and email address change

--- scripts/cmd/mail.php
+++ scripts/cmd/mail.php
@@ -40,3 +40,42 @@ function sendRegistrationMail($playerId, $username, $token, $email) {
 	$body .= "\r\n";
 	mail($email, $subject, $body, "From: ".STENDHAL_NOREPLY_EMAIL, '-f '.STENDHAL_NOREPLY_ADDRESS);
 }
+
+
+function sendChangeMail($username, $oldMail, $newMail) {
+	$subject = "Stendhal account ".$username." new mail address";
+	$body = "Hello ".$username."!\r\n";
+	$body .= "\r\n";
+	$body .= "This email confirms, that your email address was changed from ";
+	$body .= $oldMail." to \r\n";
+	$body .= $newMail.".\r\n";
+	$body .= "\r\n";
+	$body .= "\r\n";
+	$body .= "Your email address will be used for account recovery, if you forget \r\n";
+	$body .= "your password or if there are indications for an account hack.\r\n";
+	$body .= "\r\n";
+	$body .= "Any questions? Please see https://stendhalgame.org/wiki/AskForHelp\r\n";
+	$body .= "\r\n";
+	$body .= "Best regards from the Stendhal team\r\n";
+	$body .= "\r\n";
+	mail($oldMail, $subject, $body, "From: ".STENDHAL_NOREPLY_EMAIL, '-f '.STENDHAL_NOREPLY_ADDRESS);
+}
+
+
+function sendMergeMail($oldUsername, $newUsername, $oldMail) {
+	$subject = "Stendhal account ".$username." merged";
+	$body = "Hello ".$oldUsername."!\r\n";
+	$body .= "\r\n";
+	$body .= "This email confirms, that your Stendhal account ";
+	$body .= $oldUsername." was merged into ".$newUsername.".\r\n";
+	$body .= "\r\n";
+	$body .= "\r\n";
+	$body .= "Your email address will be used for account recovery, if you forget \r\n";
+	$body .= "your password or if there are indications for an account hack.\r\n";
+	$body .= "\r\n";
+	$body .= "Any questions? Please see https://stendhalgame.org/wiki/AskForHelp\r\n";
+	$body .= "\r\n";
+	$body .= "Best regards from the Stendhal team\r\n";
+	$body .= "\r\n";
+	mail($oldMail, $subject, $body, "From: ".STENDHAL_NOREPLY_EMAIL, '-f '.STENDHAL_NOREPLY_ADDRESS);
+}
