64838a373dd2fc43ee382c5c1f6430cab35f77bc
Hendrik Brummermann <nhnb@users.sourceforge.net>
2012-10-25 20:11:40 +0000

    accept reports from postman

--- content/game/sourcelog.php
+++ content/game/sourcelog.php
@@ -61,7 +61,7 @@ for ($day = 1; $day <= 31; $day++) {
 			## make it pretty, yes this code is ugly.
 			if (((strpos($line, '< CIA-') > 0) || (strpos($line, 'postman') > 0)) && (strpos($line, '> arianne_rpg: ') > 0)) {
 				
-				if (preg_match('/^[0-9][0-9]:[0-9][0-9] < CIA-.*> arianne_rpg: [^ ]*( [^ ]*)? \*/', $line)) {
+				if (preg_match('/^[0-9][0-9]:[0-9][0-9] < .*> arianne_rpg: [^ ]*( [^ ]*)? \*/', $line)) {
 					if ($res != '') {
 						echo '<li>' . $res . "</span></li>\n";
 					}
