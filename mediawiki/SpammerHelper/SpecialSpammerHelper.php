3f3c5392f9197f3070d809a24c19bb16e975fe1e
markus <madmetzger@users.sourceforge.net>
2012-11-11 11:34:53 +0000

    start implementing user tool links

--- mediawiki/SpammerHelper/SpecialSpammerHelper.php
+++ mediawiki/SpammerHelper/SpecialSpammerHelper.php
@@ -1,17 +1,18 @@
 <?php
 class SpecialSpammerHelper extends SpecialPage {
+	
         function __construct() {
                 parent::__construct( 'SpammerHelper' );
         }
  
         function execute( $par ) {
                 $this->setHeaders();
-                global $wgOut; // this is where we can put our output
+                global $wgOut, $wgUser; // this is where we can put our output
         		$wgOut->addWikiText('==Special Page to deal with notorious spammers==');
         		$wgOut->addWikiText('<p>The following table shows accounts that have more than one deleted contribution:</p>');
         		$dbr = wfGetDB( DB_SLAVE );
         		$tables = array('user', 'archive', 'ipblocks');
-        		$vars = array('user_name', 'COUNT(ar_title) archived', 'user_editcount edits');
+        		$vars = array('user_name', 'user_id', 'COUNT(ar_title) archived', 'user_editcount edits');
         		$conds = array('ipb_user IS NULL', "ar_timestamp > '20120101000000'");
         		$options = array('GROUP BY' => 'user_name', 'HAVING' => 'archived > 1 AND NOT edits > archived',
         						'ORDER BY' => 'user_name');
@@ -19,10 +20,21 @@ class SpecialSpammerHelper extends SpecialPage {
         						'ipblocks' => array('LEFT JOIN', 'user_id = ipb_user'));
         		$limit = 50;
         		$res = $dbr->select($tables, $vars, $conds, __METHOD__, $options, $join_conds);
+        		$wgOut->addWikiText('');
         		foreach ($res as $row) {
-        			$wgOut->addWikiText('<p>');
-        			$display = '<b>'.$row->user_name.'</b>';
+        			$display = $this->buildUserRow($row, $wgUser);
         			$wgOut->addWikiText($display);
         		}
+        		$wgOut->addWikiText('');
         }
+        
+        private function buildUserRow( $row, $wgUser ) {
+        	$skin = $wgUser->getSkin();
+        	$rc_user_text = $row->user_name;
+        	$rc_user = $row->user_id;
+        	$result = $rc_user_text.' ';
+        	$result .= $skin->userToolLinks( $rc_user, $rc_user_text );
+        	return $result;
+        }
+        
 }
