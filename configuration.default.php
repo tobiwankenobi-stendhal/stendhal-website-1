f4b4ecfa2ccea73ace20ae636225631866449f4c
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-03-10 19:15:09 +0000

    moved style definition for chat text into style.css

--- configuration.default.php
+++ configuration.default.php
@@ -22,7 +22,8 @@
 #
 # Please change to match your system configuration.
 #
-@define('STENDHAL_VERSION','0.79');
+@define('STENDHAL_VERSION','1.06');
+@define('STENDHAL_CACHE_BUSTER', '00000102');
 
 @define('STENDHAL_TITLE', ' &ndash; Stendhal MMORPG');
 #
