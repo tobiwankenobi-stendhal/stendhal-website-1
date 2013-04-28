569be344a4253e0b5819bc9f03fba8215873f8ed
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-04-28 20:30:51 +0000

    adjusted policy to allow faiumoni.de to embbed images from the stendhal wiki

--- index.php
+++ index.php
@@ -109,8 +109,8 @@ require_once($page_url.'.php');
 
 header('X-Frame-Options: sameorigin');
 
-$csp = "default-src 'self'; "
-	. "img-src http://ssl.gstatic.com https://ssl.gstatic.com http://csi.gstatic.com https://csi.gstatic.com http://arianne.sf.net http://arianne.sourceforge.net https://sflogo.sourceforge.net http://sflogo.sourceforge.net data: 'self'; "
+$csp = "default-src 'self' https://stendhalgame.org; "
+	. "img-src http://ssl.gstatic.com https://ssl.gstatic.com http://csi.gstatic.com https://csi.gstatic.com http://arianne.sf.net http://arianne.sourceforge.net https://sflogo.sourceforge.net http://sflogo.sourceforge.net https://stendhalgame.org data: 'self'; "
 	. "script-src https://apis.google.com https://platform.twitter.com 'self'; "
 	. "frame-src https://www.facebook.com https://plusone.google.com http://platform.twitter.com https://platform.twitter.com https://www.youtube.com 'self'; "
 	. "style-src 'self' 'unsafe-inline' ; "
