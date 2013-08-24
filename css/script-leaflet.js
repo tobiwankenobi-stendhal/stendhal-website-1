56775f96e46924f00e57f332be9cc086f5fcd341
Hendrik Brummermann <nhnb@users.sourceforge.net>
2013-08-24 05:40:33 +0000

    code cleanup of leaflet integration

--- css/script-leaflet.js
+++ css/script-leaflet.js
@@ -3,49 +3,6 @@
 	//                                 Atlas
 	//----------------------------------------------------------------------------
 /*
-	var mapType, infowindow;
-	function EuclideanProjection() {
-		var EUCLIDEAN_RANGE = 4*256; // move markers outside the map area far out of the way
-		this.pixelOrigin = new google.maps.Point(EUCLIDEAN_RANGE / 2, EUCLIDEAN_RANGE / 2);
-		this.pixelsPerLonDegree = EUCLIDEAN_RANGE / 360;
-		this.pixelsPerLonRadian = EUCLIDEAN_RANGE / (2 * Math.PI);
-		this.scaleLat = 2;      // Height - multiplication scale factor
-		this.scaleLng = 1;      // Width - multiplication scale factor
-		this.offsetLat = 0;     // Height - direct offset +/-
-		this.offsetLng = 0;     // Width - direct offset +/-
-	}
-
-	EuclideanProjection.prototype.fromLatLngToPoint = function (latLng, opt_point) {
-		var point = opt_point || new google.maps.Point(0, 0);
-		var origin = this.pixelOrigin;
-		point.x = (origin.x + (latLng.lng() + this.offsetLng ) * this.scaleLng * this.pixelsPerLonDegree);
-		point.y = (origin.y + (-1 * latLng.lat() + this.offsetLat ) * this.scaleLat * this.pixelsPerLonDegree);
-		return point;
-	};
-
-	EuclideanProjection.prototype.fromPointToLatLng = function (point) {
-		var me = this;
-		var origin = me.pixelOrigin;
-		var lng = (((point.x - origin.x) / me.pixelsPerLonDegree) / this.scaleLng) - this.offsetLng;
-		var lat = ((-1 *( point.y - origin.y) / me.pixelsPerLonDegree) / this.scaleLat) - this.offsetLat;
-		return new google.maps.LatLng(lat , lng, true);
-	};
-
-	function worldToLatLng(x, y) {
-		var xw0 = 499616;
-		var yw0 = 499744;
-		var xwz = 501280;
-		var ywz = 500896;
-
-		var xl0 = 0;
-		var yl0 = 0;
-		var xlz = 208.15;
-		var ylz = 144.2;
-
-		var lx = (x - xw0) / (xwz - xw0) * (xlz - xl0) + xl0;
-		var ly = (y - yw0) / (ywz - yw0) * (ylz - yl0) + yl0;
-		return mapType.projection.fromPointToLatLng({x:lx, y:ly});
-	}
 
 	function initializeAtlas() {
 		var tileUrlBase = $("#map_canvas").attr("data-tile-url-base");
@@ -78,29 +35,7 @@
 	}
 */
 
-	var mapX0 = -180, mapY0 = 90,  gameX0 = 499616, gameY0 = 499744;
-	var mapXZ = 124,  mapYZ = -22, gameXZ = 501280, gameYZ = 500896;
-
-	var gameXD = gameXZ - gameX0;
-	var gameYD = gameYZ - gameY0;
-	var mapXD = mapXZ = mapX0;
-	var mapYD = mapYZ - mapY0;
-
-	function gameToMap(point) {
-		var x_ = point[0] - gameX0;
-		var y_ = point[1] - gameY0;
-		
-		return [x_ * mapXD / gameXD + mapX0, y_ * mapYD / gameYD + mapY0];
-	}
-
-	function fromPointToLatLng(point) {
-		/*var x = point[0] * (360 / 255) - 180; // (0 - 255) => (-180 - 180)
-		var y = (255 - point[1]) * (180 / 255) - 90;    // (0 - 255) => (-90 - 90)
-		return [y, x];*/
-		return map.unproject(point, 0);
-	}
-
-	function worldToLatLng(point) {
+	function worldToLatLng(map, point) {
 		var x = point[0];
 		var y = point[1];
 		var xw0 = 499616;
@@ -113,14 +48,9 @@
 		var xlz = 208.15;
 		var ylz = 144.2;
 
-//		var xlz = 210;
-//		var ylz = 75;
-		
-
 		var lx = (x - xw0) / (xwz - xw0) * (xlz - xl0) + xl0;
 		var ly = (y - yw0) / (ywz - yw0) * (ylz - yl0) + yl0;
-//		console.log(x, y, "^=", lx, ly);
-		return fromPointToLatLng([lx, ly]);
+		return map.unproject(point, 0);
 	}
 
 	// http://www.netlobo.com/url_query_string_javascript.html
@@ -136,61 +66,14 @@
 	}
 
 	function initializeLeafletAtlas() {
-/*		
-		console.log(worldToLatLng([499616, 499744]));
-		console.log(worldToLatLng([500000, 500000]));
-		console.log(worldToLatLng([501280, 500896]));
-*/
+
 
 		var map = L.map('map_leaflet', {
 			attributionControl: false
 		});
-		window.map = map;
 		map.crs = L.CRS.Simple;
+		map.setView(worldToLatLng(map, [500000, 500000]), 3);
 		
-		
-		var southWest = map.unproject([0, 255], 0);
-		var northEast = map.unproject([255, 0], 0);
-		map.setMaxBounds(new L.LatLngBounds(southWest, northEast));
-		
-		map.setView(worldToLatLng([500000, 500000]), 3);
-		
-		var marker = L.marker(worldToLatLng([500000, 500000])).addTo(map);
-
-		var marker = L.marker(worldToLatLng([499100, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499200, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499300, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499400, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499500, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499600, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499700, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499800, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([499900, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500100, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500200, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500300, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500400, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500500, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500600, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500700, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500800, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500900, 500000])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499300])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499400])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499500])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499600])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499700])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499800])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 499900])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500100])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500200])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500300])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500400])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500500])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500600])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500700])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500800])).addTo(map);
-		var marker = L.marker(worldToLatLng([500000, 500900])).addTo(map);
 
 		var pois = $.parseJSON($("#data-pois").attr("data-pois"));
 		var wanted = decodeURI(gup("poi")).toLowerCase().split(",");
@@ -214,9 +97,7 @@
 				}
 			}
 		}
-		
 
-		
 		L.tileLayer('https://stendhalgame.org/map/2/{z}-{x}-{y}.png', {
 			attribution: '',
 			minZoom: 2,
