da17162e46537fd13d3354c7b6cc9510a4eeead3
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-03-03 00:04:08 +0100

    use fancybox to display screenshots

--- css/script.js
+++ css/script.js
@@ -1420,9 +1420,14 @@ easingOut:"swing",showCloseButton:true,showNavArrows:true,enableEscapeButton:tru
 		});
 
 		$('#screenshotLink').click(function (event) {
-			var left = Math.max(0, (screen.width-800)/2);
-			var top = Math.max(0, (screen.height-550)/2);
-			window.open("/images/screenshot/", "screenshot", "left="+left+",top="+top+",status=0,toolbar=0,location=0,menubar=0,directories=0,height=550,width=800,scrollbars=1");
+			$.getJSON("/?id=content/scripts/api&method=screenshots", function (data) {
+				$.fancybox(
+					data, {
+						'titlePosition'	: 'inside'
+					}
+				);
+			});
+
 			event.preventDefault();
 			return false;
 		});
@@ -1531,90 +1536,3 @@ easingOut:"swing",showCloseButton:true,showNavArrows:true,enableEscapeButton:tru
 		}
 	});
 }());
-
-
-
-
-
-
-
-
-
-$(document).ready(function() {
-	/*
-	*   Examples - images
-	*/
-
-	$("a#example1").fancybox();
-
-	$("a#example2").fancybox({
-		'overlayShow'	: false,
-		'transitionIn'	: 'elastic',
-		'transitionOut'	: 'elastic'
-	});
-
-	$("a#example3").fancybox({
-		'transitionIn'	: 'none',
-		'transitionOut'	: 'none'	
-	});
-
-	$("a#example4").fancybox({
-		'opacity'		: true,
-		'overlayShow'	: false,
-		'transitionIn'	: 'elastic',
-		'transitionOut'	: 'none'
-	});
-
-	$("a#example5").fancybox();
-
-	$("a#example6").fancybox({
-		'titlePosition'		: 'outside',
-		'overlayColor'		: '#000',
-		'overlayOpacity'	: 0.9
-	});
-
-	$("a#example7").fancybox({
-		'titlePosition'	: 'inside'
-	});
-
-	$("a#example8").fancybox({
-		'titlePosition'	: 'over'
-	});
-
-	$("a[rel=example_group]").fancybox({
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none',
-		'titlePosition' 	: 'over',
-		'titleFormat'		: function(title, currentArray, currentIndex, currentOpts) {
-			return '<span id="fancybox-title-over">Image ' + (currentIndex + 1) + ' / ' + currentArray.length + (title.length ? ' &nbsp; ' + title : '') + '</span>';
-		}
-	});
-
-	/*
-	*   Examples - various
-	*/
-
-	$("#various1").fancybox({
-		'titlePosition'		: 'inside',
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none'
-	});
-
-	$("#various2").fancybox();
-
-	$("#various3").fancybox({
-		'width'				: '75%',
-		'height'			: '75%',
-		'autoScale'			: false,
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none',
-		'type'				: 'iframe'
-	});
-
-	$("#various4").fancybox({
-		'padding'			: 0,
-		'autoScale'			: false,
-		'transitionIn'		: 'none',
-		'transitionOut'		: 'none'
-	});
-});
