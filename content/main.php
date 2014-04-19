4c0d525e02a8fd25f766840a1b7bd5f1e172cd8e
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-04-19 20:28:55 +0200

    changed box title background from wood to dark green (nolarik)

--- content/main.php
+++ content/main.php
@@ -70,7 +70,7 @@ class MainPage extends Page {
 	}
 
 	function writeContent() {
-		if (isset($_REQUEST['test'])) {
+		if (isset($_REQUEST['_layout'])) {
 			$this->writeStartPage();
 		} else {
 			$this->writeContentOld();
@@ -86,31 +86,57 @@ class MainPage extends Page {
 		echo '#contentArea {margin: 51px 0 80px 190px;}';
 		echo '#leftArea, #rightArea {display: none}';
 		echo '.box {margin-bottom: 1em}';
+		echo '.boxTitle {border:none; background-image: none; background: #0D4619}';
 		echo '</style>';
 
 		// about stendhal
 		echo '<div style="width: 55%; float: left">';
 		startBox('<h1>Stendhal</h1>');
-		echo '<p><b>Stendhal is a fun friendly and free multiplayer online adventure game. Start playing, get hooked... Get the source code, and add your own ideas...</b></p>';
+		echo '<p><b>Stendhal is a fun friendly and free multiplayer online adventure game with an old school feel.</b></p>';
 		echo '<p>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>';
  		echo '<p>Sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</p>';
 		endBox();
 
+/*
+ Start playing, get hooked... Get the source code, and add your own ideas...
+		
+		Stendhal has a huge and rich world. You can explore cities, forest, mountains, mines, plains, caves and dungeons.
+		
+		You could meet over 250 NPCs, many will give you tasks and quests for valuable experience, shiny gold, but mainly a warm fuzzy feeling. You may be asked to help protect land or people from danger, feed the hungry, heal the sick, make someone happy, solve a puzzle, play a game or simply lend a hand. You can keep track of your progress using a Travel Log.
+		
+		Your character will develop and grow and with each new level up become stronger and better. With the money you acquire you can buy new items and improve your armour and weapons, trade with friends, or look for what other players are giving away in the 'community chest'.
+		
+				If you just want to fight that helps too - the cities could get overrun by monsters without you!
+		
+				Start playing, get hooked... and if you like, get the source code, and add your own ideas!
+		
+				So what are you waiting for? A whole new world awaits...
+				Start playing, get hooked...
+		
+				With Java installed, playing is as simple as clicking Play Now on stendhalgame.org. You can play Stendhal on Windows, Mac and Linux, it is completely free and all open source.
+		
+				There is an ingame tutorial, a manual and a beginners guide.
+				Get the source code, and add your own ideas!
+		
+				Stendhal is part of the Arianne project and framework for developing games. It's a great project to get involved in, if you are interested in learning to develop Java for example. If you don't want to code you can help with graphics, history, dialogue, quest ideas, music and more. Join a team of developers from all over the world who create the game just for fun, and of course for free, in our spare time. We would really love to get your help contributing.
+		
+*/
+
 		// quick access to important pages
 		startBox('<h1>Learn more</h1>');
 		echo '<p>World Guide: ';
-		echo '<a href="'.rewriteURL('/world/atlas.html').'">Map</a>, ';
-		echo '<a href="'.rewriteURL('/region.html').'">Regions</a>, ';
+		echo '<a href="/world/atlas.html">Map</a>, ';
+		echo '<a href="/region.html">Regions</a>, ';
 		echo '<a href="/dungeon.html">Dungeons</a>, ';
-		echo '<a href="'.rewriteURL('/npc/').'">NPCs</a>, ';
+		echo '<a href="/npc/">NPCs</a>, ';
 		echo '<a href="/quest.html">Quests</a>, ';
-		echo '<a href="'.rewriteURL('/achievement.html').'">Achievements</a>, ';
-		echo '<a href="'.rewriteURL('/creature/').'">Creatures</a>, ';
-		echo '<a href="'.rewriteURL('/item/').'">Items</a></p>';
+		echo '<a href="/achievement.html">Achievements</a>, ';
+		echo '<a href="/creature/">Creatures</a>, ';
+		echo '<a href="/item/">Items</a></p>';
 
 		echo '<p>Player\'s Guide: ';
 		echo '<a href="/wiki/Stendhal_Manual">Manual</a>, ';
-		echo '<a href="/player-guide/faq.html">FAQ</a>, ';;
+		echo '<a href="/player-guide/faq.html">FAQ</a>, ';
 		echo '<a href="/player-guide/beginner-guide.html">Beginner\'s Guide</a>, ';
 		echo '<a href="/player-guide/ask-for-help.html">Ask For Help</a>, ';
 		echo '<a href="/chat/">Chat</a></p>';
