0c8e56a5324728954bda6d72361602d8c85e1ba5
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-04-22 01:10:37 +0200

    fixed media links

--- content/main.php
+++ content/main.php
@@ -138,11 +138,11 @@ class MainPage extends Page {
 
 		// screenshots and videos
 		startBox('<h1>Media</h1>');
-		echo '<p><a href="/about/screenshot.html"><img src="/images/screenshot.jpg" width="120px" height="87px"></a> ';
-		echo '<a href="/about/video.html"><img src="/images/video.jpeg" width="120px"></a> ';
+		echo '<p><a href="/media/screenshots.html"><img src="/images/screenshot.jpg" width="120px" height="87px"></a> ';
+		echo '<a href="/media/videos.html"><img src="/images/video.jpeg" width="120px"></a> ';
 // 		echo '<img src="/images/screenshot.jpg" width="120px" height="87px"> ';
 // 		echo '<img src="/images/video.jpeg" width="120px"> ';
-		echo '<p><a href="/about/screenshot.html">More images...</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="/about/video.html">More videos...</a>';
+		echo '<p><a href="/media/screenshots.html">More images...</a> &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <a href="/media/videos.html">More videos...</a>';
 		endBox();
 
 		// news
