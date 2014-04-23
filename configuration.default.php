f0fe12e507e3fde967d69c1f39ccaea6caac9909
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-04-23 23:43:33 +0200

    increased cache buster

--- configuration.default.php
+++ configuration.default.php
@@ -23,7 +23,7 @@
 # Please change to match your system configuration.
 #
 @define('STENDHAL_VERSION','1.13');
-@define('STENDHAL_CACHE_BUSTER', '00000124');
+@define('STENDHAL_CACHE_BUSTER', '00000125');
 
 @define('STENDHAL_TITLE', ' &ndash; Stendhal MMORPG');
 #
