8ca6eb40ed9f781c95a7a547fd088c8d2b15f215
Hendrik Brummermann <nhnb@users.sourceforge.net>
2014-04-23 22:53:13 +0200

    fine tuning of the download page

--- content/game/download.php
+++ content/game/download.php
@@ -12,23 +12,26 @@ class DownloadPage extends Page {
 		endBox();
 		
 		startBox('For players');
+		echo '<p>You most likely want this download:</p>';
 		echo '<ul>';
 		echo '<li><b><a href="http://arianne.sourceforge.net/download/stendhal.zip">stendhal.zip</a></b> <img src="/images/buttons/star.png"><br>Download this file to play.</li>';
 		echo '</ul>';
 		echo '<p>Stendhal works on Windows, Mac and Linux. It requires a <a href="https://java.com">Java runtime</a>.</p>';
+		echo '<p>&nbsp;</p>';
 		endBox();
 
 		startBox('For developers');
-		
+		echo '<p>If you are a developer, you may be interested in these files. Please have a look at the <a href="/development.html">development corner</a>.</p>';
 		echo '<ul>';
 		echo '<li><a href="http://arianne.sourceforge.net/download/stendhal-server.zip">stendhal-server.zip</a><br>This file contains the stendhal server files. It is not needed to play Stendhal.</li>';
 		echo '<li><a href="http://arianne.sourceforge.net/download/stendhal-src.tar.gz">stendhal-src.tar.gz</a><br>This file is for developers. It contains the source code for both the client and the server.</li>';
 		echo '</ul>';
+		echo '<p>We use <a href="http://www.mapeditor.org/download.html">Tiled</a> to edit Stendhal maps.</p>';
 		endBox();
 
 		startBox('Pre-release testing');
 		echo '<p>Please see <a href="http://stendhalgame.org/wiki/Stendhal_Testing">Stendhal Testing</a>, if you want to help us test the next release.</p>';
-		echo '<p>Use this beta version with care as it will contain bugs. Please note that it will not update itself.</p>';
+		echo '<p>Use this beta version with care as it will contain bugs. Please note: It will not update itself.</p>';
 
 		echo '<div style="margin-left: 3em">';
 
