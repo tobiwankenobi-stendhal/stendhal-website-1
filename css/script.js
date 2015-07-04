0175671611628aff52358ce23f3fe81d5fa82aef
Hendrik Brummermann <nhnb@users.sourceforge.net>
2015-07-04 19:17:04 +0200

    started to work on registering for push notifications

--- css/script.js
+++ css/script.js
@@ -1415,6 +1415,117 @@ easingOut:"swing",showCloseButton:true,showNavArrows:true,enableEscapeButton:tru
 	}
 
 	//----------------------------------------------------------------------------
+	//                                       push
+	//----------------------------------------------------------------------------
+	var isPushEnabled = false;
+	function initPush() {
+		$("#pushNotificationButton").click(function() {  
+			if (isPushEnabled) {  
+				pushUnsubscribe();  
+			} else {  
+				pushSubscribe();  
+			}
+		});
+
+		if ('serviceWorker' in navigator) {  
+			navigator.serviceWorker.register('/service-worker.js').then(initialisePushState);  
+		} else {
+			console.log('Service workers aren\'t supported in this browser.');  
+		}
+	}
+
+	// Once the service worker is registered set the initial state
+	function initialisePushState() {
+		// Are Notifications supported in the service worker?
+		if (!('showNotification' in ServiceWorkerRegistration.prototype)) {
+			console.log('Notifications aren\'t supported.');
+			return;
+		}
+
+		// Check the current Notification permission.
+		// If its denied, it's a permanent block until the
+		// user changes the permission
+		if (Notification.permission === 'denied') {
+			console.log('The user has blocked notifications.');
+			return;
+		}
+
+		// Check if push messaging is supported
+		if (!('PushManager' in window)) {
+			console.log('Push messaging isn\'t supported.');
+			return;
+		}
+
+		// We need the service worker registration to check for a subscription
+		navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+			// Do we already have a push message subscription?
+			serviceWorkerRegistration.pushManager.getSubscription()
+				.then(function(subscription) {
+					// Enable any UI which subscribes / unsubscribes from
+					// push messages.
+					$("#pushNotificationButton").removeAttr("disabled");
+
+					if (!subscription) {
+						// We aren't subscribed to push, so set UI
+						// to allow the user to enable push
+						return;
+					}
+
+				// Keep your server in sync with the latest subscriptionId
+				console.log("sendSubscriptionToServer", subscription);
+
+				// Set your UI to show they have subscribed for
+				// push messages
+				//pushButton.textContent = 'Disable Push Messages';
+				isPushEnabled = true;
+			})
+			.catch(function(err) {
+				console.warn('Error during getSubscription()', err);
+			});
+		})
+		.catch(function(err) {
+				console.warn('Error during serviceWorker.ready', err);
+			});
+	}
+
+	function pushSubscribe() {  
+		// Disable the button so it can't be changed while  
+		// we process the permission request  
+		$("#pushNotificationButton").attr("disabled", "disabled");
+
+		navigator.serviceWorker.ready.then(function(serviceWorkerRegistration) {
+			serviceWorkerRegistration.pushManager.subscribe()
+			.then(function(subscription) {
+				// The subscription was successful
+				isPushEnabled = true;
+				//pushButton.textContent = 'Disable Push Messages';
+				$("#pushNotificationButton").removeAttr("disabled");
+
+				// TODO: Send the subscription.subscriptionId and
+				// subscription.endpoint to your server
+				// and save it to send a push message at a later date
+		        return console.log("sendSubscriptionToServer", subscription);
+			})
+			.catch(function(e) {
+				if (Notification.permission === 'denied') {
+					// The user denied the notification permission which
+					// means we failed to subscribe and the user will need
+					// to manually change the notification permission to
+					// subscribe to push messages
+					console.warn('Permission for Notifications was denied');
+					$("#pushNotificationButton").attr("disabled", "disabled");
+				} else {
+					// A problem occurred with the subscription; common reasons
+					// include network errors, and lacking gcm_sender_id and/or
+					// gcm_user_visible_only in the manifest.
+					console.error('Unable to subscribe to push.', e);
+					$("#pushNotificationButton").attr("disabled", "disabled");
+				}
+			});
+		});
+	}
+
+	//----------------------------------------------------------------------------
 	//                                       init
 	//----------------------------------------------------------------------------
 
@@ -1556,4 +1667,8 @@ easingOut:"swing",showCloseButton:true,showNavArrows:true,enableEscapeButton:tru
 			audio.currentTime = 0;
 		}
 	});
+	
+	if (document.getElementById("pushNotificationButton") != null) {
+		initPush();
+	}
 }());
