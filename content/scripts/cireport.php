578ab1fb8c771f7c9e0c9d370c9c963e4df1ba60
Hendrik Brummermann <nhnb@users.sourceforge.net>
2015-12-11 23:50:46 +0100

    receive test result reports

---
+++ content/scripts/cireport.php
@@ -0,0 +1,46 @@
+<?php
+/*
+ Stendhal website - a website to manage and ease playing of Stendhal game
+ Copyright (C) 2010 the Arianne Project
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+
+class UploadPage extends Page {
+
+	public function writeHttpHeader() {
+		if (!isset($_FILES['file'])) {
+			header('HTTP/1.0 404 Not Found');
+		}
+		return true;
+	}
+
+	public function writeHtmlHeader() {
+		echo '<title>Upload'.STENDHAL_TITLE.'</title>';
+	}
+
+	function writeContent() {
+		startBox('<h1>Upload</h1>');
+		if (!isset($_FILES['file'])) {
+			echo 'Upload failed. Did you select a file?';
+		} else {
+			move_uploaded_file($_FILES['file']['tmp_name'], "/srv/upload/testresults_".date('ymd_His').'_'.$_SERVER["REMOTE_ADDR"].'.txt');
+			echo 'Thanks for you help.';
+		}
+		endBox();
+	}
+}
+$page = new UploadPage();
+?>
