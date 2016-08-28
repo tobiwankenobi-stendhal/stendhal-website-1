84de4f6eee474f772197c4393ec7071a72ca8f03
Hendrik Brummermann <nhnb@users.sourceforge.net>
2016-08-28 09:39:48 +0200

    reactivated CSP

--- index.php
+++ index.php
@@ -116,7 +116,7 @@ $csp = "default-src 'self' https://stendhalgame.org; "
 	. "frame-src https://www.facebook.com https://plusone.google.com https://apis.google.com https://platform.twitter.com https://platform.twitter.com https://www.youtube.com https://www.youtube-nocookie.com 'self'; "
 	. "style-src 'self' 'unsafe-inline'; ";
 
-// header("Content-Security-Policy: ". $csp);
+header("Content-Security-Policy: ". $csp);
 
 
 if ($page->writeHttpHeader()) {
